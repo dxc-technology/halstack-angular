9958c61e2121d53e9e381948e4b284b5b893c45f
Guillermo Vigil <guillermovigil@MacBook-Pro-de-Guillermo.local>
2019-05-07 16:02:08 +0200

    Removing animations from button module

--- projects/dxc-ngx-cdk/src/lib/dxc-button/dxc-button.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-button/dxc-button.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { DxcButtonComponent } from './dxc-button.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [DxcButtonComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [BrowserModule, MatButtonModule],
   exports: [DxcButtonComponent, MatButtonModule]
 })
 export class DxcButtonModule {}

3df2fcca5d91b33e820dd967ef5f25a37a37dc33
Guillermo Vigil <guillermovigil@MacBook-Pro-de-Guillermo.local>
2019-05-07 15:30:06 +0200

    Adding BrowserAnimationsModule

--- projects/dxc-ngx-cdk/src/lib/dxc-button/dxc-button.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-button/dxc-button.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { DxcButtonComponent } from './dxc-button.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [DxcButtonComponent],
-  imports: [BrowserModule, MatButtonModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   exports: [DxcButtonComponent, MatButtonModule]
 })
 export class DxcButtonModule {}

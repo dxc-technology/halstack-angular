2a0dd165e1606df31959a7d980348f3bd3a2eca4
Guillermo Vigil <guillermovigil@MacBook-Pro-de-Guillermo.local>
2019-05-15 09:49:15 +0200

    Adding checkbox functionality

--- projects/dxc-ngx-cdk/src/lib/dxc-checkbox/dxc-checkbox.stories.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-checkbox/dxc-checkbox.stories.ts
@@ -1,8 +1,7 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { action } from '@storybook/addon-actions';
 import { MatCheckboxModule } from '@angular/material';
 import { DxcCheckboxComponent } from './dxc-checkbox.component';
-
-export const checkbox = {};
 
 storiesOf('Checkbox', module)
   .addDecorator(
@@ -12,11 +11,21 @@ storiesOf('Checkbox', module)
     })
   )
   .add(
-    'Basic',
+    'Normal',
     () => ({
-      template: `<dxc-checkbox> Checkbox</dxc-checkbox>`,
+      template: `
+      <dxc-checkbox [(ngModel)]="checked" text="Checkbox 1" (change)="onChange($event)"></dxc-checkbox>
+      <dxc-checkbox [(ngModel)]="checked" labelPosition="before" text="Checkbox 2"></dxc-checkbox>
+      <dxc-checkbox [(ngModel)]="checked" labelPosition="after" text="Checkbox 3"></dxc-checkbox>
+      <dxc-checkbox disabled required labelPosition="before" text="Checkbox 4"></dxc-checkbox>
+      <dxc-checkbox indeterminate text="Checkbox 5"></dxc-checkbox>
+      <dxc-checkbox></dxc-checkbox>
+      <dxc-checkbox indeterminate (change)="onChange($event)"
+        (indeterminateChange)="onIndeterminateChange($event)" required text="Checkbox 7"></dxc-checkbox>
+      `,
       props: {
-        checkbox
+        onChange: action('Change fired!'),
+        onIndeterminateChange: action('Indeterminate change fired!')
       }
     }),
     { notes: 'Normal DXC Checkbox' }

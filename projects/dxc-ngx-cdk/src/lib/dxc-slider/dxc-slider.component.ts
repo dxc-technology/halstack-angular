b06caaf95ff42104720022e91d56582df69635fc
Saez Alvarez <asaezalvarez@dxc.com>
2019-06-07 09:40:30 +0200

    adding slider

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-slider/dxc-slider.component.ts
@@ -0,0 +1,82 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  HostBinding,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { Validators, FormControl } from '@angular/forms';
+
+@Component({
+  selector: "dxc-slider",
+  templateUrl: "./dxc-slider.component.html",
+  changeDetection:ChangeDetectionStrategy.OnPush,
+  styleUrls: [
+    "./dxc-slider.component.scss",
+    "./dxc-light-slider.scss",
+    "./dxc-dark-slider.scss"
+  ]
+})
+export class DxcSliderComponent implements OnInit {
+  @HostBinding("class.dxc-light") isLight: boolean = true;
+  @HostBinding("class.dxc-dark") isDark: boolean = false;
+
+  //Default values
+  @Input() min: number = 0;
+  @Input() max: number = 100;
+  @Input() step: number = 1;
+  @Input() showLimitValues: boolean = false;
+  @Input() showInput: boolean = false;
+  @Input() value: number = 0;
+  @Input() theme: string = "light";
+
+  @Input() name: string;
+  @Input() disabled: boolean;
+  @Input() required: boolean;
+
+  @Output() dragEnd: EventEmitter<any> = new EventEmitter<any>();
+  @Output() valueChange: EventEmitter<any> = new EventEmitter<any>();
+  @Output() inputBlur: EventEmitter<any> = new EventEmitter<any>();
+  tickInterval: any;
+  ngOnInit(): void {
+    this.tickInterval = this.step > 1 ? 1 : 0;
+    if (this.theme === "dark") {
+      this.isLight = false;
+      this.isDark = true;
+    }
+  }
+
+  /**
+   * Executed while  slider value moves or when user press a key in input
+   *  @param $event
+   */
+  public valueChanged($event: any): void {
+    let  newValue = $event.value || $event.target.value;
+    if (newValue > this.max) {
+      newValue = this.max;
+    }
+    if (newValue< this.min) {
+      newValue = this.min;
+    }
+
+    this.valueChange.emit(newValue);
+  }
+
+  /**
+   *  Eexecuted once the user has ended dragging
+   * @param $event
+   */
+  public mouseUp($event): void {
+    this.dragEnd.emit( $event.value);
+  }
+
+  /**
+   *Executed when input lost the focus
+   */
+  public onBlur($event): void {
+   
+    this.inputBlur.emit( $event.target.value);
+  }
+}
--- projects/dxc-ngx-cdk/src/public-api.ts
+++ projects/dxc-ngx-cdk/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/dxc-button/dxc-button.module';
 export * from './lib/dxc-checkbox/dxc-checkbox.module';
 export * from './lib/dxc-date/dxc-date.module';
+export * from './lib/dxc-slider/dxc-slider.module';

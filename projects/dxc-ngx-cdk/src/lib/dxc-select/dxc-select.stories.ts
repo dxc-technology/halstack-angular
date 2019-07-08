19696d1c05af257299ca714b997ea08832d2b877
Saez Alvarez <asaezalvarez@dxc.com>
2019-07-08 08:43:34 +0200

    adding styles to select and icons

--- projects/dxc-ngx-cdk/src/lib/dxc-select/dxc-select.stories.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-select/dxc-select.stories.ts
@@ -6,6 +6,10 @@ import { boolean, select, array, text } from "@storybook/addon-knobs";
 import 'hammerjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+const defaultValue = [{label:'Macaw',value: 'macaw',iconSrc:'./macaw.svg'},{label:'Toucan',value: 'toucan', iconSrc:'/toucan.svg'}];
+let noIcons = [{label:'Macaw',value: 'macaw'},{label:'Toucan',value: 'toucan'}];
+const iconsText = [{label:'Macaw',value: 'macaw',iconSrc:'./macaw.svg'},{label:'Toucan',value: 'toucan', iconSrc:'/toucan.svg'}];
+const icons = [{value: 'macaw',iconSrc:'./macaw.svg'},{value:'toucan',iconSrc:'/toucan.svg'}];
 
 
 storiesOf("Form Components|Select", module)
@@ -17,6 +21,7 @@ storiesOf("Form Components|Select", module)
   .add(
     'Types',
     () => ({
+      
       template: `
       <h3 style="display: block;
       font-size: 1.17em;
@@ -26,14 +31,49 @@ storiesOf("Form Components|Select", module)
       margin-inline-end: 0px;
       font-weight: bold;"
       >Light</h3>
-      <div  style="
-      width: calc(100% - 380px); 
-      margin-right: 200px;
-      padding: 20px;
-      margin-left: 45px;">
 
+        <div  style="
+        width: calc(100% - 380px); 
+        margin-right: 200px;
+        padding: 20px;
+        margin-left: 45px;">
+
+            <dxc-select 
+            [label] = "'Simple select'"
+            [options] = "options"
+            ></dxc-select>
+
+            <dxc-select 
+            [label] = "'disabled select'"
+            [options] = "options"
+            [disabled] = true
+            ></dxc-select>
+
+            <dxc-select 
+            [multiple]= true
+            [label] = "'Multiple Select'"
+            [options] = "options"
+            ></dxc-select>
+
+            <dxc-select 
+            [multiple] = true
+            [label] = "'Multiple Select with text   & icons'"
+            [options] = "optionsTextIcons"
+            ></dxc-select>
+            
+            <dxc-select 
+            [multiple] = true
+            [label] = "'Multiple Select with text   & icons'"
+            [options] = "optionsTextIcons"
+            ></dxc-select>
+
+            <dxc-select 
+            [label] = "'Multiple Select with only  icons'"
+            [multiple] = true
+            [options] = "optionsOnlyIcons"
+            ></dxc-select>
    
-        
+        </div>
   
      
 
@@ -56,17 +96,21 @@ storiesOf("Form Components|Select", module)
 
     </div>
       `,
+
       props: {
-        valueChange: action('Change fired!'),
-        dragEnd: action('Drag end change fired!')
+        options: array("Options", noIcons),
+        optionsOnlyIcons: array("OptionsOnlyIcons", icons),
+        optionsTextIcons: array("OptionsTextAndIcon", iconsText),
+
+        valueChange: action('Change fired!')
       }
     }),
     {
     notes: {markdown: selectMD }
     }
   );
-  const defaultValue = [{label:'Macaw',value: 'macaw',iconSrc:'./macaw.svg'},{label:'Toucan',value: 'toucan', iconSrc:'/toucan.svg'}];
-    storiesOf("Form Components|Select", module)
+ 
+  storiesOf("Form Components|Select", module)
     .addDecorator(
       moduleMetadata({
         imports: [DXCSelectModule,BrowserModule, BrowserAnimationsModule],

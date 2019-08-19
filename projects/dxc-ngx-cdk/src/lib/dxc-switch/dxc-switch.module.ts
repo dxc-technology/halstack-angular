ff118f97bd78f8ae520facaec6761bc1e126302d
Cheng <jcheng42@dxc.com>
2019-08-19 10:12:06 +0200

    Switch

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-switch/dxc-switch.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { DxcSwitchComponent } from './dxc-switch.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [DxcSwitchComponent],
+  imports: [CommonModule, MatSlideToggleModule, FormsModule],
+  exports: [DxcSwitchComponent]
+})
+export class DxcSwitchModule {}

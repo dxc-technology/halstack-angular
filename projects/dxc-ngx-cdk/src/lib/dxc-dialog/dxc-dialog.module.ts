f44dd1b0b3166befdbd8af9e0cc47b91f6f4b7ac
Cheng <jcheng42@dxc.com>
2019-09-24 13:41:35 +0200

    dialog

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-dialog/dxc-dialog.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DxcDialogComponent } from './dxc-dialog.component';
+
+@NgModule({
+  declarations: [DxcDialogComponent],
+  imports: [CommonModule],
+  exports: [DxcDialogComponent]
+})
+export class DXCDialogModule {}
---
+++ projects/dxc-ngx-cdk/src/lib/dxc-dialog/dxc-dialog.stories.ts
@@ -0,0 +1,75 @@
+import { storiesOf, moduleMetadata } from "@storybook/angular";
+import dialogMD from "./README.md";
+import { boolean, array, text, object } from "@storybook/addon-knobs";
+import { DXCDialogModule } from "./dxc-dialog.module";
+import { DxcButtonModule } from "../dxc-button/dxc-button.module";
+
+let visible1 = false;
+let visible2 = false;
+storiesOf("Form Components|Dialog", module)
+  .addDecorator(
+    moduleMetadata({
+      imports: [DXCDialogModule, DxcButtonModule]
+    })
+  )
+  .add(
+    "Types",
+    () => ({
+      template: `
+      <div>
+        <dxc-button label="click me" (onClick)="onClick($event)"></dxc-button>
+        <dxc-dialog [overlay]="false" [isVisible]="visible1" (onClose)="onClick($event)">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit curae, sodales ac sapien consequat augue vitae id suscipit rhoncus, nunc auctor nisl faucibus mattis dapibus sociis. Senectus sociis viverra laoreet ligula habitant nullam potenti lectus tortor sodales, vulputate semper erat mauris fringilla natoque diam praesent scelerisque eget, mattis facilisi dapibus nibh interdum donec condimentum venenatis sollicitudin. Sociosqu taciti class diam placerat aliquam imperdiet tempus, metus natoque euismod convallis dictum ac dapibus suscipit, a molestie pulvinar gravida viverra faucibus. Dis ad quis tincidunt facilisi neque nunc, ligula odio rhoncus non nibh elementum, massa lacinia mollis inceptos molestie.
+        </dxc-dialog>
+
+        <dxc-button label="click me" (onClick)="onClick2($event)"></dxc-button>
+        <dxc-dialog [overlay]="true" [isVisible]="visible2" (onClose)="onClick2($event)">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit curae, sodales ac sapien consequat augue vitae id suscipit rhoncus, nunc auctor nisl faucibus mattis dapibus sociis. Senectus sociis viverra laoreet ligula habitant nullam potenti lectus tortor sodales, vulputate semper erat mauris fringilla natoque diam praesent scelerisque eget, mattis facilisi dapibus nibh interdum donec condimentum venenatis sollicitudin. Sociosqu taciti class diam placerat aliquam imperdiet tempus, metus natoque euismod convallis dictum ac dapibus suscipit, a molestie pulvinar gravida viverra faucibus. Dis ad quis tincidunt facilisi neque nunc, ligula odio rhoncus non nibh elementum, massa lacinia mollis inceptos molestie.
+        </dxc-dialog>
+      </div>
+      `,
+
+      props: {
+        onClick: function( event ) {
+          this.visible1 = !this.visible1;
+        },
+        onClick2: function( event ) {
+          this.visible2 = !this.visible2;
+        }
+      }
+    }),
+    {
+      notes: { markdown: dialogMD }
+    }
+  );
+
+storiesOf("Form Components|Dialog", module)
+  .addDecorator(
+    moduleMetadata({
+      imports: [DXCDialogModule, DxcButtonModule]
+    })
+  )
+  .add(
+    "Knobs example",
+    () => ({
+      template: `
+      <div >
+        <dxc-button label="click me" (onClick)="onClick($event)"></dxc-button>
+        <dxc-dialog [isCloseVisible]="isCloseVisible" [overlay]="overlay" [isVisible]="visible1" (onClose)="onClick($event)">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit curae, sodales ac sapien consequat augue vitae id suscipit rhoncus, nunc auctor nisl faucibus mattis dapibus sociis. Senectus sociis viverra laoreet ligula habitant nullam potenti lectus tortor sodales, vulputate semper erat mauris fringilla natoque diam praesent scelerisque eget, mattis facilisi dapibus nibh interdum donec condimentum venenatis sollicitudin. Sociosqu taciti class diam placerat aliquam imperdiet tempus, metus natoque euismod convallis dictum ac dapibus suscipit, a molestie pulvinar gravida viverra faucibus. Dis ad quis tincidunt facilisi neque nunc, ligula odio rhoncus non nibh elementum, massa lacinia mollis inceptos molestie.
+        </dxc-dialog>
+      </div>
+      `,
+      props: {
+        overlay: boolean("Overlay", false),
+        isCloseVisible: boolean("Is Close Visible", false),
+        visible1: boolean("Is Visible", false),
+        onClick: function( event ) {
+          this.visible1 = !this.visible1;
+        }
+      }
+    }),
+    {
+      notes: { markdown: dialogMD }
+    }
+  );

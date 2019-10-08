b659a0d9ca9e6d02a7f567aadb087ec53f1dc4f9
Cheng <jcheng42@dxc.com>
2019-10-08 14:05:04 +0200

    spinner

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-spinner/dxc-spinner.component.ts
@@ -0,0 +1,48 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  HostBinding,
+  Output,
+  EventEmitter
+} from "@angular/core";
+
+@Component({
+  selector: "dxc-spinner",
+  templateUrl: "./dxc-spinner.component.html",
+  styleUrls: [
+    "./dxc-spinner.component.scss",
+    "./dxc-light-spinner.component.scss",
+    "./dxc-dark-spinner.component.scss"
+  ]
+})
+export class DxcSpinnerComponent {
+  mode: string = "indeterminate";
+  @Input() theme: string = "light";
+  @Input() value: string;
+  @Input() label: string;
+  @Input() showValue: boolean = false;
+  @Input() overlay: boolean;
+
+  @HostBinding("class.light") isLight: boolean = true;
+  @HostBinding("class.dark") isDark: boolean = false;
+
+  public ngOnChanges(): void {
+    if (this.theme === "dark") {
+      this.isLight = false;
+      this.isDark = true;
+    } else {
+      this.isLight = true;
+      this.isDark = false;
+    }
+    if (this.value) {
+      this.mode = "determinate";
+    }
+  }
+
+  public ngOnInit(): void {
+    if (this.value) {
+      this.mode = "determinate";
+    }
+  }
+}

70cceadb989f21bed9ffdae3d9b718935fabbab1
Cheng <jcheng42@dxc.com>
2019-10-09 12:57:54 +0200

    progressbar

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-progressbar/dxc-progressbar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material';
+import { DxcProgressbarComponent } from './dxc-progressbar.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [DxcProgressbarComponent],
+  imports: [CommonModule, MatProgressBarModule],
+  exports: [DxcProgressbarComponent]
+})
+export class DxcProgressbarModule {}

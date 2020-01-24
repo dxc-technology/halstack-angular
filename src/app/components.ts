a087116a6bdf5ef19e3cbf02531ac491a138ec97
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2020-01-24 14:21:45 +0100

    rever to commit fe84cf6643eef6b512705013f69e03c43e624f25

--- src/app/components.ts
+++ src/app/components.ts
@@ -25,6 +25,7 @@ import { DxcUploadComponent } from '../../projects/dxc-ngx-cdk/src/lib/dxc-uploa
 import { AccordionComponent } from './pages/accordion/accordion.component';
 import { AlertComponent } from './pages/alert/alert.component';
 import { BoxComponent } from './pages/box/box.component';
+import { AllComponent } from './pages/all/all.component';
 
 
 export const Components = [
@@ -127,6 +128,11 @@ export const Components = [
         name: 'Upload',
         route: 'upload',
         component: DxcUploadComponent
+    },
+    {
+        name: 'All',
+        route: 'all',
+        component: AllComponent
     }
    
 ]

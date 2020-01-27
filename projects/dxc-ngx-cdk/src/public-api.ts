a5753ca992f5731242f8ef643133e8e65bfedc87
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2020-01-27 13:27:18 +0100

    add exports to other components and theme directive

--- projects/dxc-ngx-cdk/src/public-api.ts
+++ projects/dxc-ngx-cdk/src/public-api.ts
@@ -2,14 +2,24 @@
  * Public API Surface of dxc-ngx-cdk
  */
 
+export * from './lib/dxc-accordion/dxc-accordion.module';
+export * from './lib/dxc-alert/dxc-alert.module';
 export * from './lib/dxc-button/dxc-button.module';
+export * from './lib/dxc-card/dxc-card.module';
 export * from './lib/dxc-checkbox/dxc-checkbox.module';
 export * from './lib/dxc-date/dxc-date.module';
-export * from './lib/dxc-radio/dxc-radio.module';
-export * from './lib/dxc-slider/dxc-slider.module';
-export * from './lib/dxc-select/dxc-select.module';
+export * from './lib/dxc-dialog/dxc-dialog.module';
 export * from './lib/dxc-dropdown/dxc-dropdown.module';
-export * from './lib/dxc-tabs/dxc-tabs.module';
-export * from './lib/dxc-switch/dxc-switch.module';
+export * from './lib/dxc-footer/dxc-footer.module';
 export * from './lib/dxc-header/dxc-header.module';
-export * from './lib/dxc-card/dxc-card.module';
+export * from './lib/dxc-progressbar/dxc-progressbar.module';
+export * from './lib/dxc-radio/dxc-radio.module';
+export * from './lib/dxc-select/dxc-select.module';
+export * from './lib/dxc-slider/dxc-slider.module';
+export * from './lib/dxc-spinner/dxc-spinner.module';
+export * from './lib/dxc-switch/dxc-switch.module';
+export * from './lib/dxc-tabs/dxc-tabs.module';
+export * from './lib/dxc-text-input/dxc-input-text.module';
+export * from './lib/dxc-toggle/dxc-toggle.module';
+export * from './lib/dxc-upload/dxc-upload.module';
+export * from './lib/theme/theme.module'

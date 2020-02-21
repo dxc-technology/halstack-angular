9d80199aca0ef6944293832fb9de26a526890ac5
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2020-02-21 12:55:04 +0100

    applying changes for library generation

--- projects/dxc-ngx-cdk/src/public-api.ts
+++ projects/dxc-ngx-cdk/src/public-api.ts
@@ -6,32 +6,32 @@ export * from './lib/theme/theme.service';
 export * from './lib/theme/theme.directive';
 export * from './lib/theme/theme.module';
 
-export * from './lib/dxc-accordion/dxc-accordion.component';
-export * from './lib/dxc-accordion/dxc-accordion.module';
+// export * from './lib/dxc-accordion/dxc-accordion.component';
+// export * from './lib/dxc-accordion/dxc-accordion.module';
 
-export * from './lib/dxc-alert/dxc-alert.component';
-export * from './lib/dxc-alert/dxc-alert.module';
+// export * from './lib/dxc-alert/dxc-alert.component';
+// export * from './lib/dxc-alert/dxc-alert.module';
 
-export * from './lib/dxc-box/dxc-box.component';
-export * from './lib/dxc-box/dxc-box.module';
+// export * from './lib/dxc-box/dxc-box.component';
+// export * from './lib/dxc-box/dxc-box.module';
 
 export * from './lib/dxc-button/dxc-button.component';
 export * from './lib/dxc-button/dxc-button.module';
 
-export * from './lib/dxc-card/dxc-card.component';
-export * from './lib/dxc-card/dxc-card.module';
+// export * from './lib/dxc-card/dxc-card.component';
+// export * from './lib/dxc-card/dxc-card.module';
 
-export * from './lib/dxc-checkbox/dxc-checkbox.component';
-export * from './lib/dxc-checkbox/dxc-checkbox.module';
+// export * from './lib/dxc-checkbox/dxc-checkbox.component';
+// export * from './lib/dxc-checkbox/dxc-checkbox.module';
 
-export * from './lib/dxc-date/dxc-date.component';
-export * from './lib/dxc-date/dxc-date.module';
+// export * from './lib/dxc-date/dxc-date.component';
+// export * from './lib/dxc-date/dxc-date.module';
 
-export * from './lib/dxc-dialog/dxc-dialog.component';
-export * from './lib/dxc-dialog/dxc-dialog.module';
+// export * from './lib/dxc-dialog/dxc-dialog.component';
+// export * from './lib/dxc-dialog/dxc-dialog.module';
 
-export * from './lib/dxc-dropdown/dxc-dropdown.component';
-export * from './lib/dxc-dropdown/dxc-dropdown.module';
+// export * from './lib/dxc-dropdown/dxc-dropdown.component';
+// export * from './lib/dxc-dropdown/dxc-dropdown.module';
 
 export * from './lib/dxc-footer/dxc-footer.component';
 export * from './lib/dxc-footer/dxc-footer.module';
@@ -39,32 +39,32 @@ export * from './lib/dxc-footer/dxc-footer.module';
 export * from './lib/dxc-header/dxc-header.component';
 export * from './lib/dxc-header/dxc-header.module';
 
-export * from './lib/dxc-progressbar/dxc-progressbar.component';
-export * from './lib/dxc-progressbar/dxc-progressbar.module';
+// export * from './lib/dxc-progressbar/dxc-progressbar.component';
+// export * from './lib/dxc-progressbar/dxc-progressbar.module';
 
-export * from './lib/dxc-radio/dxc-radio.component';
-export * from './lib/dxc-radio/dxc-radio.module';
+// export * from './lib/dxc-radio/dxc-radio.component';
+// export * from './lib/dxc-radio/dxc-radio.module';
 
-export * from './lib/dxc-select/dxc-select.component';
-export * from './lib/dxc-select/dxc-select.module';
+// export * from './lib/dxc-select/dxc-select.component';
+// export * from './lib/dxc-select/dxc-select.module';
 
-export * from './lib/dxc-slider/dxc-slider.component';
-export * from './lib/dxc-slider/dxc-slider.module';
+// export * from './lib/dxc-slider/dxc-slider.component';
+// export * from './lib/dxc-slider/dxc-slider.module';
 
-export * from './lib/dxc-spinner/dxc-spinner.component';
-export * from './lib/dxc-spinner/dxc-spinner.module';
+// export * from './lib/dxc-spinner/dxc-spinner.component';
+// export * from './lib/dxc-spinner/dxc-spinner.module';
 
-export * from './lib/dxc-switch/dxc-switch.component';
-export * from './lib/dxc-switch/dxc-switch.module';
+// export * from './lib/dxc-switch/dxc-switch.component';
+// export * from './lib/dxc-switch/dxc-switch.module';
 
 // export * from './lib/dxc-tabs/dxc-tabs.component';
 // export * from './lib/dxc-tabs/dxc-tabs.module';
 
-export * from './lib/dxc-text-input/dxc-input-text.component';
-export * from './lib/dxc-text-input/dxc-input-text.module';
+// export * from './lib/dxc-text-input/dxc-input-text.component';
+// export * from './lib/dxc-text-input/dxc-input-text.module';
 
-export * from './lib/dxc-toggle/dxc-toggle.component';
-export * from './lib/dxc-toggle/dxc-toggle.module';
+// export * from './lib/dxc-toggle/dxc-toggle.component';
+// export * from './lib/dxc-toggle/dxc-toggle.module';
 
 // export * from './lib/dxc-upload/dxc-upload.component';
 // export * from './lib/dxc-upload/dxc-upload.module';

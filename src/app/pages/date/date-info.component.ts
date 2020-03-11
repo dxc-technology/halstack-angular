38c884a730278d01bd48bad6c71aa96294dc6606
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2020-03-11 09:32:43 +0100

    simplify how to check is a date is valid or not

--- src/app/pages/date/date-info.component.ts
+++ src/app/pages/date/date-info.component.ts
@@ -10,15 +10,12 @@ export class DateInfoComponent  implements OnInit{
 
   inputValue:Date;
 
-  validDate = true;
-  
   ngOnInit(): void {
     this.inputValue = new Date("1995/12/03");
   }
 
   onChange(event)  {
-    this.validDate = event.isValid;
-    this.inputValue = this.validDate ?  new Date(event) : null;
+    this.inputValue = event.isValid ?  new Date(event) : null;
   };
   constructor()  {}
 

90c33ffdb8a67436d0dc2e07bcd34053a0b17680
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2020-03-18 11:23:15 +0100

    changes in dropdown

--- projects/dxc-ngx-cdk/src/lib/dxc-footer/dxc-footer.component.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-footer/dxc-footer.component.ts
@@ -22,6 +22,8 @@ export class DxcFooterComponent implements OnChanges {
   @Input() public bottomLinks: { href?: string; text?: string }[];
 
   @Input() public copyright: string;
+  @Input() margin: any;
+  @Input() padding: any;  
   @Input() public logoSrc: string;
 
   defaultImglogo: string;
@@ -30,13 +32,69 @@ export class DxcFooterComponent implements OnChanges {
     socialLinks: {},
     bottomLinks: {},
     copyright: "",
-    logoSrc: null
+    logoSrc: null,
+    margin:null,
+    padding: null
   });
+
+  // Styling
+  footerContainerStyle:string;
+  footerHeaderStyle: string;
+  socialIconsStyle: string = css`  
+    display: flex;
+    align-items: center;
+  `;
+
+  socialIconStyle: string = css`
+    display: inline-flex;
+  `;
+
+  socialIconImageStyle: string = css`
+    display: inline-flex;
+    height: 25px;
+    width: 25px;
+  `;
+
+  childComponentsStyle: string;
+
+  footerFooterStyle:string;
+
+  socialLinksStyle: string = css`
+    display: inline-flex;
+    margin-left: 15px;
+    &:first-child {
+      margin-left: 0px;
+    }
+  `;
+
+  bottomLinksStyle:string = css`
+      padding-top: 6px;
+      border-top: 2px solid #ffed00;
+      display: inline-flex;
+      flex-wrap: wrap;
+      max-width: 60%;
+      .point {
+        margin: 0px 10px;
+        color: white;
+      }
+      a {
+        text-decoration: none;
+        color: white;
+        font-size: 12px;
+      }
+  `;
+  copyrightStyle: string = css`
+    color: white;
+    font-size: 12px;
+    max-width: 40%;
+  `;
 
   constructor(private utils: CssUtils) {}
 
   public ngOnInit() {
-    this.className = `${this.getDynamicStyle(this.defaultInputs.getValue())}`;
+    this.footerContainerStyle = `${this.setFooterContainerStyle(this.defaultInputs.getValue())}`;
+    this.childComponentsStyle = `${this.setChildComponentsStyle(this.defaultInputs.getValue())}`;
+    this.footerHeaderStyle = `${this.setFooterFooterStyle(this.defaultInputs.getValue())}`;
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
@@ -45,83 +103,126 @@ export class DxcFooterComponent implements OnChanges {
       return result;
     }, {});
     this.defaultInputs.next({ ...this.defaultInputs.getValue(), ...inputs });
-    this.className = `${this.getDynamicStyle(this.defaultInputs.getValue())}`;
+    this.footerContainerStyle = `${this.setFooterContainerStyle(this.defaultInputs.getValue())}`;
+    this.childComponentsStyle = `${this.setChildComponentsStyle(this.defaultInputs.getValue())}`;
+    this.footerHeaderStyle = `${this.setFooterFooterStyle(this.defaultInputs.getValue())}`;
+
   }
 
-  getDynamicStyle(inputs) {
+
+  setFooterContainerStyle(input: any){
     return css`
-      max-width: 100%;
-      display: block;
-      mat-toolbar {
-        overflow: hidden;
-        ${this.utils.getTopMargin(inputs.margin)}
-        font: unset;
-        
-        mat-toolbar-row {
-          height: auto;
-          padding: 0px;
-          white-space: inherit;
-          
-        }
         padding: 20px 60px 20px 20px;
         font-family: "Open Sans", sans-serif;
         background-color: black;
-        .footerHeader {
-          display: flex;
-          justify-content: space-between;
-          flex-wrap: wrap;
-          .logo {
-            height: 34px;
-            width: auto;
-          }
-          
-          .socialIcons {
-            display: flex;
-            align-items: center;
-            .socialIcon {
-              display: inline-flex;
-            }
-          }
-          .socialIconImage {
-            display: inline-flex;
-            height: 25px;
-            width: 25px;
-          }
-          .spacing {
-            margin-left: 15px;
-          }
-        }
-        .childComponents {
-          min-height: 15px;
-          color: #fff;
-        }
-        .footerFooter {
-          display: flex;
-          justify-content: space-between;
-          align-items: baseline;
-          .bottomLinks {
-            padding-top: 6px;
-            border-top: 2px solid #ffed00;
-            display: inline-flex;
-            flex-wrap: wrap;
-            max-width: 60%;
-            .point {
-              margin: 0px 10px;
-              color: white;
-            }
-            a {
-              text-decoration: none;
-              color: white;
-              font-size: 12px;
-            }
-          }
-          .copyright {
-            color: white;
-            font-size: 12px;
-            max-width: 40%;
-          }
-        }
-      }
+        max-width: 100%;
+        ${this.utils.getTopMargin(input.margin)}    
     `;
   }
+
+  setFooterHeaderStyle(inputs: any){
+    return css`
+      display: flex;
+      justify-content: space-between;
+    `;
+  }
+
+  setChildComponentsStyle(inputs: any){
+    return css`
+      min-height: 15px;
+      color: #fff;
+      overflow: hidden;
+      ${this.utils.getPaddings(inputs.padding)}
+    `;
+  }
+
+  setFooterFooterStyle(inputs:any){
+    return css`
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+      flex-direction: row;
+    `;
+  }
+
+  
+  // getDynamicStyle(inputs) {
+  //   return css`
+  //     max-width: 100%;
+  //     display: block;
+  //     mat-toolbar {
+  //       overflow: hidden;
+  //       ${this.utils.getTopMargin(inputs.margin)}
+  //       font: unset;
+        
+  //       mat-toolbar-row {
+  //         height: auto;
+  //         padding: 0px;
+  //         white-space: inherit;
+          
+  //       }
+  //       padding: 20px 60px 20px 20px;
+  //       font-family: "Open Sans", sans-serif;
+  //       background-color: black;
+  //       .footerHeader {
+  //         display: flex;
+  //         justify-content: space-between;
+  //         flex-wrap: wrap;
+  //         .logo {
+  //           height: 34px;
+  //           width: auto;
+  //         }
+          
+  //         .socialIcons {
+  //           display: flex;
+  //           align-items: center;
+  //           .socialIcon {
+  //             display: inline-flex;
+  //           }
+  //         }
+  //         .socialIconImage {
+  //           display: inline-flex;
+  //           height: 25px;
+  //           width: 25px;
+  //         }
+  //         .spacing {
+  //           margin-left: 15px;
+  //         }
+  //       }
+  //       .childComponents {
+  //         min-height: 15px;
+  //         color: #fff;
+  //         overflow: hidden;
+  //         ${this.utils.getPaddings(inputs.padding)}
+
+  //       }
+  //       .footerFooter {
+  //         display: flex;
+  //         justify-content: space-between;
+  //         align-items: baseline;
+  //         .bottomLinks {
+  //           padding-top: 6px;
+  //           border-top: 2px solid #ffed00;
+  //           display: inline-flex;
+  //           flex-wrap: wrap;
+  //           max-width: 60%;
+  //           .point {
+  //             margin: 0px 10px;
+  //             color: white;
+  //           }
+  //           a {
+  //             text-decoration: none;
+  //             color: white;
+  //             font-size: 12px;
+  //           }
+  //         }
+  //         .copyright {
+  //           color: white;
+  //           font-size: 12px;
+  //           max-width: 40%;
+  //         }
+  //       }
+  //     }
+  //   `;
+  // }
 }

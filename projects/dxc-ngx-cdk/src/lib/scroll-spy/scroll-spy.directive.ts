b0620f4f9bfeca793e3c0c01cb93810540c2799e
Victor Rodriguez <vrodriguezgu@dxc.com>
2020-03-24 08:08:32 +0100

    modified position and track logic

--- projects/dxc-ngx-cdk/src/lib/scroll-spy/scroll-spy.directive.ts
+++ projects/dxc-ngx-cdk/src/lib/scroll-spy/scroll-spy.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, EventEmitter, Output, ElementRef } from '@angular/core';
+;import { Directive, Input, EventEmitter, Output, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[scroll-spy]',
@@ -10,8 +10,11 @@ export class ScrollSpyDirective {
    @Input() public spiedTags = [];
   
    @Output() public sectionChange = new EventEmitter<string>();
-  
-   private currentSection: string;
+
+  private TABS_HEIGHT : number = 54;
+  private HEADER_OFFSET : number = 65;
+
+  private currentSection: string;
 
   constructor(private _el: ElementRef) {
 
@@ -23,15 +26,15 @@ export class ScrollSpyDirective {
 
       const children = this._el.nativeElement.children;
       const scrollTop = event.target.scrollingElement.scrollTop;
-      const parentOffset = event.target.scrollingElement.offsetTop + 50;
+      const parentOffset = event.target.scrollingElement.offsetTop + this.HEADER_OFFSET + this.TABS_HEIGHT + 5;
+
       for (let i = 0; i < children.length; i++) {
           const element = children[i];
+          
           if (this.spiedTags.some(spiedTag => spiedTag === element.tagName)) {
               if ((element.offsetTop - parentOffset) <= scrollTop) {
                 currentSection =  element.id;
-              } else {
-                currentSection = '';
-              }
+              } 
           }
       }
       if (currentSection !== this.currentSection && currentSection !== undefined) {

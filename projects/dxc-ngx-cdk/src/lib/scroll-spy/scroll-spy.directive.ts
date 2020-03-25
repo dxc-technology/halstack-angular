9749c740e3fe4786d6575bc9748d940e4da87f0f
Victor Rodriguez <vrodriguezgu@dxc.com>
2020-03-25 11:08:00 +0100

    parametrized scroll offset for active section detection

--- projects/dxc-ngx-cdk/src/lib/scroll-spy/scroll-spy.directive.ts
+++ projects/dxc-ngx-cdk/src/lib/scroll-spy/scroll-spy.directive.ts
@@ -7,39 +7,35 @@
 })
 export class ScrollSpyDirective {
 
-   @Input() public spiedTags = [];
-  
-   @Output() public sectionChange = new EventEmitter<string>();
-
-  private TABS_HEIGHT : number = 54;
-  private HEADER_OFFSET : number = 65;
+  @Input() public spiedTags = [];
+  @Input() public offset : number = 0;;
+ 
+  @Output() public sectionChange = new EventEmitter<string>();
 
   private currentSection: string;
 
-  constructor(private _el: ElementRef) {
-
-  }
+  constructor(private _el: ElementRef) {}
 
   track(event: any) {
-
+    
     let currentSection: string;
 
-      const children = this._el.nativeElement.children;
-      const scrollTop = event.target.scrollingElement.scrollTop;
-      const parentOffset = event.target.scrollingElement.offsetTop + this.HEADER_OFFSET + this.TABS_HEIGHT + 5;
+    const children = this._el.nativeElement.children;
+    const scrollTop = event.target.scrollingElement.scrollTop;
+    const parentOffset = event.target.scrollingElement.offsetTop + this.offset;
 
-      for (let i = 0; i < children.length; i++) {
-          const element = children[i];
-          
-          if (this.spiedTags.some(spiedTag => spiedTag === element.tagName)) {
-              if ((element.offsetTop - parentOffset) <= scrollTop) {
-                currentSection =  element.id;
-              } 
-          }
-      }
-      if (currentSection !== this.currentSection && currentSection !== undefined) {
-          this.currentSection = currentSection.substr(currentSection.length - 1);
-          this.sectionChange.emit(this.currentSection);
-      }
+    for (let i = 0; i < children.length; i++) {
+        const element = children[i];      
+        if (this.spiedTags.some(spiedTag => spiedTag === element.tagName)) {
+          if ((element.offsetTop - parentOffset - 5) <= scrollTop) {
+            currentSection =  element.id;
+          } 
+        }
+     }
+
+    if (currentSection !== this.currentSection && currentSection !== undefined) {
+      this.currentSection = currentSection.substr(currentSection.length - 1);
+      this.sectionChange.emit(this.currentSection);
+    }
   }
 }

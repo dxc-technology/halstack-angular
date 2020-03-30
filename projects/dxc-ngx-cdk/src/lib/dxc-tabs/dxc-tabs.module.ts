ae6e2b26f23e995f969130e1c033d33fd959f306
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2020-03-30 19:21:36 +0200

    changes color by vat and add theme dir in comp

--- projects/dxc-ngx-cdk/src/lib/dxc-tabs/dxc-tabs.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-tabs/dxc-tabs.module.ts
@@ -10,6 +10,8 @@ import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { DxcTabsComponent } from './dxc-tabs.component';
 import { DxcTabComponent } from './dxc-tab/dxc-tab.component';
+import { ThemeModule } from '../theme/theme.module';
+import { defaultTheme } from '../theme/defaultTheme';
 
 @NgModule({
   declarations: [DxcTabsComponent, DxcTabComponent],
@@ -18,7 +20,11 @@ import { DxcTabComponent } from './dxc-tab/dxc-tab.component';
     MatInputModule,
     MatTabsModule,
     MatFormFieldModule,
-    FormsModule
+    FormsModule,
+    ThemeModule.forRoot({
+      themes: [defaultTheme],
+      active: "default"
+    })
   ],
   exports: [DxcTabsComponent,DxcTabComponent],
   entryComponents:[MatTab,MatTabGroup]

5a25c955ea5f1a2012bf1df921dfa9ce2b59fafa
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-04-29 13:01:46 +0200

    Fixed bug card theme

--- projects/dxc-ngx-cdk/src/lib/dxc-card/dxc-card.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-card/dxc-card.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material';
 import { DxcCardComponent } from './dxc-card.component';
 import { CommonModule } from '@angular/common';
+import { ThemeModule, defaultTheme } from "../theme";
 
 @NgModule({
   declarations: [DxcCardComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule, 
+    MatCardModule,
+    ThemeModule.forRoot({
+      themes: [defaultTheme],
+      active: "default"
+    })
+  ],
   exports: [DxcCardComponent]
 })
 export class DxcCardModule {}

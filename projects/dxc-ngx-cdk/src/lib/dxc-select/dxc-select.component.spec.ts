f78e3ae7678de75f8f00a055e0fdea1448d89c4a
Cheng <jcheng42@dxc.com>
2020-05-12 09:37:35 +0200

    select multiple tests

--- projects/dxc-ngx-cdk/src/lib/dxc-select/dxc-select.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-select/dxc-select.component.spec.ts
@@ -105,4 +105,55 @@ describe("DxcSelect tests", () => {
     expect(changeMock).toHaveBeenCalledWith("3");
     expect(dxcSelect.getByText("Amazon"));
   });
+
+  test("dxc-select uncontrolled multiple functionality", async () => {
+    const changeMock = jest.fn();
+    const dxcSelect = await render(DxcSelectComponent, {
+      componentProperties: {
+        label: "test-select",
+        options: optionsMock,
+        multiple: true,
+        onChange: { emit: changeMock } as any
+      },
+      imports: [MatSelectModule]
+    });
+    dxcSelect.detectChanges();
+    fireEvent.click(dxcSelect.getByText("test-select"));
+    dxcSelect.detectChanges();
+    fireEvent.click(screen.getByText("Apple"));
+    expect(changeMock).toHaveBeenCalledWith(["3"]);
+    fireEvent.click(screen.getByText("Google"));
+    expect(changeMock).toHaveBeenCalledWith(["3", "4"]);
+    dxcSelect.detectChanges();
+    expect(dxcSelect.getByText("Apple,Google"));
+  });
+
+  test("dxc-select controlled multiple functionality", async () => {
+    const changeMock = jest.fn(x=> {
+      console.log(x);
+      dxcSelect.detectChanges();
+    });
+    const dxcSelect = await render(DxcSelectComponent, {
+      componentProperties: {
+        label: "test-select",
+        options: optionsMock,
+        multiple: true,
+        value: ["1", "2"],
+        onChange: { emit: changeMock } as any
+      },
+      imports: [MatSelectModule]
+    });
+    dxcSelect.detectChanges();
+    dxcSelect.getByText("Amazon,Ebay");
+    fireEvent.click(dxcSelect.getByText("Amazon,Ebay"));
+    dxcSelect.detectChanges();
+    fireEvent.click(screen.getByText("Apple"));
+    expect(changeMock).toHaveBeenCalledWith(["1", "2", "3"]);
+    dxcSelect.detectChanges();
+    fireEvent.click(screen.getByText("Google"));
+    expect(changeMock).toHaveBeenCalledWith(["1", "2", "3", "4"]);
+    dxcSelect.detectChanges();
+    expect(dxcSelect.getByText("Amazon,Ebay"));
+  });
+
 });

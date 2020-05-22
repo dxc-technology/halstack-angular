f0d75395377c5d456156d198cee94b175fdec350
Cheng <jcheng42@dxc.com>
2020-05-22 13:02:07 +0200

    OC-8861-Tests for the Alert component

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-alert/dxc-alert.component.spec.ts
@@ -0,0 +1,64 @@
+import { render, fireEvent } from '@testing-library/angular';
+import { screen } from '@testing-library/dom';
+import { DxcAlertComponent } from './dxc-alert.component'
+
+describe('DxcAlertComponent tests', () => {
+  test('should render default dxc-alert', async () => {
+    const projection = "Content inside the ng-content!";
+    const dxcAlert = await render(DxcAlertComponent, {
+      template: `<dxc-alert>${projection}</dxc-alert>`,
+      componentProperties: { }
+    })
+
+    expect(dxcAlert.getByText(projection));
+    expect(dxcAlert.getByText("INFORMATION"));
+  })
+
+  test('should render dxc-alert warning', async () => {
+    const projection = "Content inside the ng-content!";
+    const dxcAlert = await render(DxcAlertComponent, {
+      template: `<dxc-alert type="warning">${projection}</dxc-alert>`,
+      componentProperties: { }
+    })
+
+    expect(dxcAlert.getByText(projection));
+    expect(dxcAlert.getByText("WARNING"));
+  })
+  
+  test('should render dxc-alert error', async () => {
+    const projection = "Content inside the ng-content!";
+    const dxcAlert = await render(DxcAlertComponent, {
+      template: `<dxc-alert type="error">${projection}</dxc-alert>`,
+      componentProperties: { }
+    })
+
+    expect(dxcAlert.getByText(projection));
+    expect(dxcAlert.getByText("ERROR"));
+  })
+
+  test('should render dxc-alert confirm', async () => {
+    const projection = "Content inside the ng-content!";
+    const dxcAlert = await render(DxcAlertComponent, {
+      template: `<dxc-alert type="confirm">${projection}</dxc-alert>`,
+      componentProperties: { }
+    })
+
+    expect(dxcAlert.getByText(projection));
+    expect(dxcAlert.getByText("SUCCESS"));
+  })
+
+  test('should dxc-alert call onClose', async () => {
+    const onCloseFunction = jest.fn();
+    const projection = "Content inside the ng-content!";
+    const dxcAlert = await render(DxcAlertComponent, {
+      template: `<dxc-alert (onClose)="onCloseFunction()">${projection}</dxc-alert>`,
+      componentProperties: { onCloseFunction }
+    })
+
+    expect(dxcAlert.getByText(projection));
+    const closeIcon = dxcAlert.getByTestId("closeIcon");
+    
+    fireEvent.click(closeIcon);
+    expect(onCloseFunction).toHaveBeenCalled();
+  })
+})

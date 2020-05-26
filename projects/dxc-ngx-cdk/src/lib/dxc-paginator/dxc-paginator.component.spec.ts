133f3e3523d9ccb738b4ffe2292175eb2b65d649
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-05-26 14:02:05 +0200

    More tests paginator

--- projects/dxc-ngx-cdk/src/lib/dxc-paginator/dxc-paginator.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-paginator/dxc-paginator.component.spec.ts
@@ -6,7 +6,7 @@ import { DxcButtonModule } from '../dxc-button/dxc-button.module';
 
 describe("DxcPaginator tests", () => {
 
-  test("should render dxc-paginator", async () => {
+  test("should render default dxc-paginator", async () => {
     const paginator = await render(DxcPaginatorComponent, {
       template: `<dxc-paginator></dxc-paginator>`,
       componentProperties: {},
@@ -56,23 +56,185 @@ describe("DxcPaginator tests", () => {
     expect(paginator.getByText("Page: 2 of 3")).toBeTruthy();
   });
 
-  /*test("should click next function", async () => {
+  test("should click next function", async () => {
     const navegateFunction = jest.fn();
-    const paginationActions : Array<string> = ['prev', 'next', 'first'];
-    const page : number = 2;
+    const page : number = 1;
     const totalItems : number = 27;
     const itemsPerPage : number = 10;
-    const actions = ['next'];
     const paginator = await render(DxcPaginatorComponent, {
-      template: `<dxc-paginator [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]=${actions} (nextFunction)="navegateFunction($event, 'next')"></dxc-paginator>`,
+      template: `<dxc-paginator (nextFunction)="navegateFunction($event,'next')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['next']" ></dxc-paginator>`,
       componentProperties: {
-        nextFunction: {navegateFunction }
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    expect(paginator.getByText("1 to 10 of 27")).toBeTruthy();
+    const nextButton = paginator.getAllByRole("button");
+    fireEvent.click(nextButton[0]);
+    expect(navegateFunction).toHaveBeenCalled();
+  });
+
+  test("should click prev function", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 2;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (prevFunction)="navegateFunction($event,'prev')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['prev']" ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const btn = paginator.getAllByRole("button");
+    fireEvent.click(btn[0]);
+    expect(navegateFunction).toHaveBeenCalled();
+  });
+
+  test("should click first function", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 2;
+    const totalItems : number = 30;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (firstFunction)="navegateFunction($event,'first')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['first']" ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const btn = paginator.getAllByRole("button");
+    fireEvent.click(btn[0]);
+    expect(navegateFunction).toHaveBeenCalled();
+  });
+
+  test("should click last function", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 1;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (lastFunction)="navegateFunction($event,'last')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['last']" ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const btn = paginator.getAllByRole("button");
+    fireEvent.click(btn[0]);
+    expect(navegateFunction).toHaveBeenCalled();
+  });
+
+  test("should click disabled next function", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 2;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (nextFunction)="navegateFunction($event,'next')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['next']" ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
       },
       imports: [DxcButtonModule]
     });
     const nextButton = paginator.getAllByRole("button");
-    fireEvent.click(nextButton[0]);
-    expect(navegateFunction).toHaveBeenCalled();
-  });*/
+    expect(nextButton[0].hasAttribute("disabled")).toBe(true);
+  });
+
+  test("should click disabled prev function", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 1;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (prevFunction)="navegateFunction($event,'prev')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['prev']" ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const btn = paginator.getAllByRole("button");
+    expect(btn[0].hasAttribute("disabled")).toBe(true);
+  });
+
+  test("should click disabled first function", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 1;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (firstFunction)="navegateFunction($event,'first')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['first']" ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const btn = paginator.getAllByRole("button");
+    expect(btn[0].hasAttribute("disabled")).toBe(true);
+  });
+
+  test("should click last function", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 2;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (lastFunction)="navegateFunction($event,'last')" [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} [paginationActions]="['last']" ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const btn = paginator.getAllByRole("button");
+    expect(btn[0].hasAttribute("disabled")).toBe(true);
+  });
+
+  test("should disable last and next buttons", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 2;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (nextFunction)="navegateFunction($event,'next')" (lastFunction)="navegateFunction($event,'last')" 
+      (firstFunction)="navegateFunction($event,'first')" (prevFunction)="navegateFunction($event,'prev')" 
+      [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const firstButton = paginator.getAllByRole("button")[0];
+    const prevButton = paginator.getAllByRole("button")[1];
+    const nextButton = paginator.getAllByRole("button")[2];
+    const lastButton = paginator.getAllByRole("button")[3];
+    expect(firstButton.hasAttribute("disabled")).toBeFalsy();
+    expect(prevButton.hasAttribute("disabled")).toBeFalsy();
+    expect(nextButton.hasAttribute("disabled")).toBeTruthy();
+    expect(lastButton.hasAttribute("disabled")).toBeTruthy();
+  });
+
+  test("should disable first and previous buttons", async () => {
+    const navegateFunction = jest.fn();
+    const page : number = 1;
+    const totalItems : number = 20;
+    const itemsPerPage : number = 10;
+    const paginator = await render(DxcPaginatorComponent, {
+      template: `<dxc-paginator (nextFunction)="navegateFunction($event,'next')" (lastFunction)="navegateFunction($event,'last')" 
+      (firstFunction)="navegateFunction($event,'first')" (prevFunction)="navegateFunction($event,'prev')" 
+      [itemsPerPage]=${itemsPerPage} [totalItems]=${totalItems} [currentPage]=${page} ></dxc-paginator>`,
+      componentProperties: {
+        navegateFunction
+      },
+      imports: [DxcButtonModule]
+    });
+    const firstButton = paginator.getAllByRole("button")[0];
+    const prevButton = paginator.getAllByRole("button")[1];
+    const nextButton = paginator.getAllByRole("button")[2];
+    const lastButton = paginator.getAllByRole("button")[3];
+    expect(firstButton.hasAttribute("disabled")).toBeTruthy();
+    expect(prevButton.hasAttribute("disabled")).toBeTruthy();
+    expect(nextButton.hasAttribute("disabled")).toBeFalsy();
+    expect(lastButton.hasAttribute("disabled")).toBeFalsy();
+  });
 
 });

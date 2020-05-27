4ceccdd67fa3536d5a7f39ac443a5d137eef227e
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-05-27 11:59:47 +0200

    Test table

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-table/dxc-table.component.spec.ts
@@ -0,0 +1,42 @@
+import { render, fireEvent } from "@testing-library/angular";
+import { screen } from "@testing-library/dom";
+import { DxcTableComponent } from "./dxc-table.component";
+import { TestBed } from "@angular/core/testing";
+import { DxcTableModule } from './dxc-table.module';
+
+describe("DxcTable tests", () => {
+  test("should render dxc-table", async () => {
+    const table = await render(DxcTableComponent, {
+      template: `<dxc-table margin="medium">
+                    <tr>
+                        <th>header 1</th>
+                        <th>header 2</th>
+                        <th>header 3</th>
+                    </tr>
+                    <tr>
+                        <td>cell 1</td>
+                        <td>cell 2</td>
+                        <td>cell 3</td>
+                    </tr>
+                    <tr>
+                        <td>cell 4</td>
+                        <td>cell 5</td>
+                        <td>cell 6</td>
+                    </tr>
+                    </dxc-table>`,
+      componentProperties: { },
+      imports: [DxcTableModule],
+      excludeComponentDeclaration: true
+    });
+    table.detectChanges();
+    expect(table.getByText("header 1")).toBeTruthy();
+    expect(table.getByText("header 2")).toBeTruthy();
+    expect(table.getByText("header 3")).toBeTruthy();
+    expect(table.getByText("cell 1")).toBeTruthy();
+    expect(table.getByText("cell 2")).toBeTruthy();
+    expect(table.getByText("cell 3")).toBeTruthy();
+    expect(table.getByText("cell 4")).toBeTruthy();
+    expect(table.getByText("cell 5")).toBeTruthy();
+    expect(table.getByText("cell 6")).toBeTruthy();
+    });
+});

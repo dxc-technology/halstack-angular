cbdccfb195fe2ae0ea0242bacd63c04bbba88747
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-07-07 16:23:06 +0200

    Directive for closable elements

--- projects/dxc-ngx-cdk/src/lib/dxc-header/dxc-header.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-header/dxc-header.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material";
 import { CommonModule } from "@angular/common";
 import { DxcHeaderComponent } from "./dxc-header.component";
+import { ClosableDirective } from './directives/closable.directive';
 
 @NgModule({
-  declarations: [DxcHeaderComponent],
+  declarations: [DxcHeaderComponent, ClosableDirective],
   imports: [
     CommonModule,
     MatToolbarModule
   ],
-  exports: [DxcHeaderComponent]
+  exports: [DxcHeaderComponent,ClosableDirective]
 })
 export class DXCHeaderModule {}

96bea1606dcf4a6aa39c7a4c82f0d999a483bad6
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-07-21 17:46:54 +0200

    Added header responsive template and directive template

---
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/header/header-directives/header-directives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'header-directives',
+  templateUrl: './header-directives.component.html',
+  styleUrls: ['./header-directives.component.scss']
+})
+export class HeaderDirectivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

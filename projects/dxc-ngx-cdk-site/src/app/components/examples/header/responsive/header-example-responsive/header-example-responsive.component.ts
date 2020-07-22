138594b2a949e6001d71ea039524bcb2420d893f
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-07-22 13:35:28 +0200

    Added responsive example and deleted responsive component

--- projects/dxc-ngx-cdk-site/src/app/components/examples/header/responsive/header-example-responsive/header-example-responsive.component.ts
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/header/responsive/header-example-responsive/header-example-responsive.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { Example } from '../../../../../model/example';
-import { ExampleService } from '../../../../../service/example.service';
-import { HeaderResponsiveComponent } from '../header-responsive/header-responsive.component';
 
 @Component({
   selector: 'header-example-responsive',
@@ -10,32 +7,25 @@ import { HeaderResponsiveComponent } from '../header-responsive/header-responsiv
 })
 export class HeaderExampleResponsiveComponent implements OnInit {
 
-  examples: Array<Example>;
+  bindCode = `
+  <dxc-header>
+    <div id="unresponsive">
+        <a>Overview</a>
+        <a>Components</a>
+    </div>
+    <div id="responsive">
+      <ul style="list-style: none;">
+        <li><a>Overview</a></li>
+        <li><a>Components</a></li>
+      </ul>
+    </div>
+  </dxc-header>
+  `;
 
-  constructor(@Inject("ExampleService") private exampleService: ExampleService) { 
-    this.examples =  new Array();
+  constructor() { 
   }
 
   ngOnInit(): void {
-    this.generateExamples();
   }
-
-  private generateExamples() {
-
-    this.exampleService
-      .getCodeExample("header/responsive/header-responsive/header-responsive.component").subscribe(resp1 => {
-        this.examples.push(this.exampleService.generateExample({
-          title: 'Responsive Header',
-          component: HeaderResponsiveComponent,
-          selector: "example5",
-          examples: [
-            resp1[0],
-            resp1[1],
-            resp1[2]
-          ]
-        }));
-      });
-
-    }
 
 }

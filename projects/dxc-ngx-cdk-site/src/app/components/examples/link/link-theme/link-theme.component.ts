6dc6941a1f3e73b505973cc36c999620a0b85971
Parrilla <m.parrilla@dxc.com>
2020-08-11 14:03:49 +0200

    Updated site

---
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/link/link-theme/link-theme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'link-theme',
+  templateUrl: './link-theme.component.html',
+  styleUrls: ['./link-theme.component.scss']
+})
+export class LinkThemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

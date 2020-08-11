be0abf7785fa4e5071cdfdb697953a7ce604b850
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-08-11 16:18:38 +0200

    Switch: added theming into site

---
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/switch/switch-api/switch-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'switch-api',
+  templateUrl: './switch-api.component.html',
+  styleUrls: ['./switch-api.component.scss']
+})
+export class SwitchApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

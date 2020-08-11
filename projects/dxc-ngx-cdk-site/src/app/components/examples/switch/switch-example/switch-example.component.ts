25b1e25cc2591a98a61d31c908f7929f7a568931
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-08-11 16:18:38 +0200

    Switch: added theming into site

--- projects/dxc-ngx-cdk-site/src/app/components/examples/switch/switch-example/switch-example.component.ts
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/switch/switch-example/switch-example.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Inject, ChangeDetectorRef } from "@angular/core";
 import { Example } from "src/app/model/example";
 import { ExampleService } from "src/app/service/example.service";
 import { SwitchLabelComponent } from '../switch-label/switch-label.component';
-import { SwitchThemedComponent } from "../switch-themed/switch-themed.component";
 import { SwitchUncontrolledComponent } from '../switch-uncontrolled/switch-uncontrolled.component';
 import { SwitchDefaultComponent } from '../switch-default/switch-default.component';
 
@@ -55,20 +54,6 @@ export class SwitchExampleComponent implements OnInit {
         })
       );
     });
-
-    this.exampleService
-      .getCodeExample("switch/switch-themed/switch-themed.component")
-      .subscribe(resp1 => {
-        this.examples.push(
-          this.exampleService.generateExample({
-            title: "Themed Switch",
-            component: SwitchThemedComponent,
-            selector: "example3",
-            examples: [resp1[0], resp1[1], resp1[2]]
-          })
-        );
-      });
-
       this.exampleService
       .getCodeExample("switch/switch-label/switch-label.component")
       .subscribe(resp1 => {
@@ -76,7 +61,7 @@ export class SwitchExampleComponent implements OnInit {
           this.exampleService.generateExample({
             title: "Label Position Switch",
             component: SwitchLabelComponent,
-            selector: "example4",
+            selector: "example3",
             examples: [resp1[0], resp1[1], resp1[2]]
           })
         );

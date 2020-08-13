348a05440cb9efa954f2caa4fa1a285b49eea08c
Parrilla <m.parrilla@dxc.com>
2020-08-13 11:40:15 +0200

    Site updated

---
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/tag/tag-theme/tag-theme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-theme',
+  templateUrl: './tag-theme.component.html',
+  styleUrls: ['./tag-theme.component.scss']
+})
+export class TagThemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

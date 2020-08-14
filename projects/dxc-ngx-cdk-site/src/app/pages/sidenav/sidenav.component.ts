190456d37d6e12d1cbe66d9f46c1656739b7b327
Parrilla <m.parrilla@dxc.com>
2020-08-14 09:33:20 +0200

    Site updated

--- projects/dxc-ngx-cdk-site/src/app/pages/sidenav/sidenav.component.ts
+++ projects/dxc-ngx-cdk-site/src/app/pages/sidenav/sidenav.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Section } from "../../model/sections";
 import { SidenavTablePropertiesComponent } from "../../components/examples/sidenav/properties/sidenav-table-properties/sidenav-table-properties.component";
 import { SidenavExampleComponent } from "../../components/examples/sidenav/sidenav-example/sidenav-example.component";
-import { SidenavImportComponent } from '../../components/examples/sidenav/sidenav-import/sidenav-import.component';
+import { SidenavImportComponent } from "../../components/examples/sidenav/sidenav-import/sidenav-import.component";
+import { SidenavApiComponent } from "../../components/examples/sidenav/sidenav-api/sidenav-api.component";
+import { SidenavThemeComponent } from "../../components/examples/sidenav/sidenav-theme/sidenav-theme.component";
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  selector: "app-sidenav",
+  templateUrl: "./sidenav.component.html",
+  styleUrls: ["./sidenav.component.scss"],
 })
 export class SidenavComponent implements OnInit {
-
   sections: Array<Section>;
   tagImg: string;
 
-  constructor(){
+  constructor() {
     this.sections = new Array<Section>();
-    this.tagImg = './assets/img/ready.svg';
+    this.tagImg = "./assets/img/ready.svg";
   }
 
   ngOnInit() {
     this.sections.push(
       {
         id: 0,
-        label: "PROPERTIES",
-        selector: "sidenav-table-properties",
-        component: SidenavTablePropertiesComponent
+        label: "API",
+        selector: "sidenav-api",
+        component: SidenavApiComponent,
       },
       {
         id: 1,
-        label: "MODULE",
-        selector: "sidenav-import",
-        component: SidenavImportComponent
+        label: "THEMING",
+        selector: "sidenav-theme",
+        component: SidenavThemeComponent,
       },
       {
         id: 2,
         label: "EXAMPLES",
         selector: "sidenav-example",
-        component: SidenavExampleComponent
+        component: SidenavExampleComponent,
       }
     );
   }
-
 }

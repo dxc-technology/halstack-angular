14263622d26ddebd6dfc53cb3f08941550b0e635
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-08-17 15:05:50 +0200

    Footer: added theming into site

---
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/footer/footer-theme/footer-theme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'footer-theme',
+  templateUrl: './footer-theme.component.html',
+  styleUrls: ['./footer-theme.component.scss']
+})
+export class FooterThemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

e4e3175f00396eb2b8fb22a7e30aa92c53644c3d
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-08-19 12:08:37 +0200

    Dialog: added thmeing into site

---
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/dialog/dialog-theme/dialog-theme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-theme',
+  templateUrl: './dialog-theme.component.html',
+  styleUrls: ['./dialog-theme.component.scss']
+})
+export class DialogThemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

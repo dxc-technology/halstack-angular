90cf63807e3c7b39fcc503e9fff9ea2acad0f621
Parrilla <m.parrilla@dxc.com>
2020-08-19 13:49:26 +0200

    Added opacity tokens

--- projects/dxc-ngx-cdk/src/lib/theme/theme.service.ts
+++ projects/dxc-ngx-cdk/src/lib/theme/theme.service.ts
@@ -44,9 +44,6 @@ export class ThemeService {
     if(theme.properties["sidenav"].hasOwnProperty("arrowBackground")){
       this.theme.properties["sidenav"]["arrowBackground"] = theme.properties["sidenav"]["arrowBackground"] + "80";
     }
-    if(theme.properties["progressbar"].hasOwnProperty("color")){
-      this.theme.properties["progressbar"]["color"] = theme.properties["progressbar"]["color"] + "57";
-    }
     if(theme.properties["autocomplete"].hasOwnProperty("selectedOptionBackgroundColor")){
       this.theme.properties["autocomplete"]["hoverOptionBackgroundColor"] = theme.properties["autocomplete"]["selectedOptionBackgroundColor"] + "57";
     }

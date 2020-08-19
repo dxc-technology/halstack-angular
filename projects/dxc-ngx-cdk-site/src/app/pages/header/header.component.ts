bfa9637f119c50e5831eeb6bd103602f7a2adfd0
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-08-19 11:34:29 +0200

    Header: changed site header page

--- projects/dxc-ngx-cdk-site/src/app/pages/header/header.component.ts
+++ projects/dxc-ngx-cdk-site/src/app/pages/header/header.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Section } from '../../model/sections';
-import { HeaderTablePropertiesComponent } from '../../components/examples/header/properties/header-table-properties/header-table-properties.component';
 import { HeaderExampleComponent } from '../../components/examples/header/header-example/header-example.component';
-import { HeaderDirectivesComponent } from '../../components/examples/header/header-directives/header-directives.component';
-import { HeaderExampleResponsiveComponent } from '../../components/examples/header/responsive/header-example-responsive/header-example-responsive.component';
-import { HeaderImportComponent } from '../../components/examples/header/header-import/header-import.component';
+import { HeaderApiComponent } from '../../components/examples/header/header-api/header-api.component';
+import { HeaderThemeComponent } from '../../components/examples/header/header-theme/header-theme.component';
 
 @Component({
   selector: 'app-header',
@@ -22,14 +20,10 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.sections.push( 
-      {id:0, label: 'PROPERTIES',selector: 'examples-properties-header', 
-        component: HeaderTablePropertiesComponent},
-      {id:1, label: 'MODULE',selector: 'header-import', 
-        component: HeaderImportComponent},
-      {id:2, label: 'DIRECTIVES',selector: 'header-directives', 
-        component: HeaderDirectivesComponent},
-      {id:3, label: 'RESPONSIVE',selector: 'header-example-responsive', 
-        component: HeaderExampleResponsiveComponent},
+      {id:0, label: 'API',selector: 'header-api', 
+        component: HeaderApiComponent},
+      {id:1, label: 'THEMING',selector: 'header-theme', 
+        component: HeaderThemeComponent},
       {id:4, label: 'EXAMPLES', selector:'examples-components-header',component: HeaderExampleComponent});
   }
 

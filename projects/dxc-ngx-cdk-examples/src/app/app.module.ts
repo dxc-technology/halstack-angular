2474e55e5b67546876aa3539953cffeaf06a5bd6
Parrilla <m.parrilla@dxc.com>
2020-09-01 17:03:13 +0200

    Added site page

--- projects/dxc-ngx-cdk-examples/src/app/app.module.ts
+++ projects/dxc-ngx-cdk-examples/src/app/app.module.ts
@@ -1,48 +1,76 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ButtonInfoComponent } from './pages/button/button-info.component';
-import { SliderInfoComponent } from './pages/slider/slider-info.component';
-import { CheckboxInfoComponent } from './pages/checkbox/checkbox-info.component';
-import { DateInfoComponent } from './pages/date/date-info.component';
-import { DialogInfoComponent } from './pages/dialog/dialog-info.component';
-import { HeaderInfoComponent } from './pages/header/header-info.component';
-import { ToggleInfoComponent } from './pages/toggle/toggle-info.component';
-import { TextInputInfoComponent } from './pages/inputText/text-input-info.component';
-import { SelectInfoComponent } from './pages/select/select-info.component';
-import { SpinnerInfoComponent } from './pages/spinner/spinner.component';
-import { SwitchInfoComponent } from './pages/switch/switch-info.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RadioInfoComponent } from './pages/radio/radio-info.component';
-import { DropdownInfoComponent } from './pages/dropdown/dropdown-info.component';
-import { FooterInfoComponent } from './pages/footer/footer-info.component';
-import { AccordionComponent } from './pages/accordion/accordion.component';
-import { AlertComponent } from './pages/alert/alert.component';
-import { BoxComponent } from './pages/box/box.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AllComponent } from './pages/all/all.component';
-import { TableInfoComponent } from './pages/table/table-info.component';
-import { CardInfoComponent } from './pages/card/card-info.component';
-import { TagInfoComponent } from './pages/tag-info/tag-info.component';
-import { ProgressInfoComponent } from './pages/progressBar/progressBar-info.component';
-import { TabbedSectionComponent } from './pages/tabbed-section/tabbed-section.component';
-import { TabsInfoComponent } from './pages/tabs/tabs-info.component';
-import { PaginatorComponent } from './pages/paginator/paginator.component';
-import { LinkInfoComponent } from './pages/link/link-info.component';
-import { SidenavInfoComponent } from './pages/sidenav/sidenav-info.component';
-import { WizardComponent } from './pages/wizard/wizard.component';
-import { HeadingInfoComponent } from './pages/heading/heading-info.component';
-import { ResultsetTableComponent } from './pages/resultset-table/resultset-table.component';
-import { CommonModule } from '@angular/common';
-import { DxcAccordionModule, DXCAlertModule, DxcBoxModule, DxcButtonModule, DxcCardModule, 
-  DxcCheckboxModule, DXCDialogModule, DXCDropdownModule, 
-  DXCFooterModule, DXCHeaderModule, DXCInputTextModule, DxcProgressbarModule, 
-  DXCSelectModule, DxcSpinnerModule, DxcSwitchModule, DXCToggleModule, 
-  DxcTabsModule, DXCUploadModule, DxcTableModule, DxcDateModule, DxcTagModule, DxcRadioModule, 
-  DxcSliderModule, DxcTabbedSectionModule,
-  DxcSideNavModule, DxcPaginatorModule, DxcLinkModule, ThemeService, DxcWizardModule, DxcHeadingModule,DxcResultsetTableModule } from '@dxc-technology/halstack-angular';
-import { UploadComponent } from './pages/upload/upload.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ButtonInfoComponent } from "./pages/button/button-info.component";
+import { SliderInfoComponent } from "./pages/slider/slider-info.component";
+import { CheckboxInfoComponent } from "./pages/checkbox/checkbox-info.component";
+import { DateInfoComponent } from "./pages/date/date-info.component";
+import { DialogInfoComponent } from "./pages/dialog/dialog-info.component";
+import { HeaderInfoComponent } from "./pages/header/header-info.component";
+import { ToggleInfoComponent } from "./pages/toggle/toggle-info.component";
+import { TextInputInfoComponent } from "./pages/inputText/text-input-info.component";
+import { SelectInfoComponent } from "./pages/select/select-info.component";
+import { SpinnerInfoComponent } from "./pages/spinner/spinner.component";
+import { SwitchInfoComponent } from "./pages/switch/switch-info.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RadioInfoComponent } from "./pages/radio/radio-info.component";
+import { DropdownInfoComponent } from "./pages/dropdown/dropdown-info.component";
+import { FooterInfoComponent } from "./pages/footer/footer-info.component";
+import { AccordionComponent } from "./pages/accordion/accordion.component";
+import { AlertComponent } from "./pages/alert/alert.component";
+import { BoxComponent } from "./pages/box/box.component";
+import { HttpClientModule } from "@angular/common/http";
+import { AllComponent } from "./pages/all/all.component";
+import { TableInfoComponent } from "./pages/table/table-info.component";
+import { CardInfoComponent } from "./pages/card/card-info.component";
+import { TagInfoComponent } from "./pages/tag-info/tag-info.component";
+import { ProgressInfoComponent } from "./pages/progressBar/progressBar-info.component";
+import { TabbedSectionComponent } from "./pages/tabbed-section/tabbed-section.component";
+import { TabsInfoComponent } from "./pages/tabs/tabs-info.component";
+import { PaginatorComponent } from "./pages/paginator/paginator.component";
+import { LinkInfoComponent } from "./pages/link/link-info.component";
+import { SidenavInfoComponent } from "./pages/sidenav/sidenav-info.component";
+import { WizardComponent } from "./pages/wizard/wizard.component";
+import { HeadingInfoComponent } from "./pages/heading/heading-info.component";
+import { ResultsetTableComponent } from "./pages/resultset-table/resultset-table.component";
+import { CommonModule } from "@angular/common";
+import {
+  DxcAccordionModule,
+  DXCAlertModule,
+  DxcBoxModule,
+  DxcButtonModule,
+  DxcCardModule,
+  DxcCheckboxModule,
+  DXCDialogModule,
+  DXCDropdownModule,
+  DXCFooterModule,
+  DXCHeaderModule,
+  DXCInputTextModule,
+  DxcProgressbarModule,
+  DXCSelectModule,
+  DxcSpinnerModule,
+  DxcSwitchModule,
+  DXCToggleModule,
+  DxcTabsModule,
+  DXCUploadModule,
+  DxcTableModule,
+  DxcDateModule,
+  DxcTagModule,
+  DxcRadioModule,
+  DxcSliderModule,
+  DxcTabbedSectionModule,
+  DxcSideNavModule,
+  DxcPaginatorModule,
+  DxcLinkModule,
+  ThemeService,
+  DxcWizardModule,
+  DxcHeadingModule,
+  DxcResultsetTableModule,
+  DxcTextareaModule,
+} from "@dxc-technology/halstack-angular";
+import { UploadComponent } from "./pages/upload/upload.component";
+import { TextareaInfoComponent } from "./pages/textarea/textarea-info.component";
 
 @NgModule({
   declarations: [
@@ -62,6 +90,7 @@ import { UploadComponent } from './pages/upload/upload.component';
     DropdownInfoComponent,
     FooterInfoComponent,
     TextInputInfoComponent,
+    TextareaInfoComponent,
     SelectInfoComponent,
     SidenavInfoComponent,
     SpinnerInfoComponent,
@@ -77,7 +106,7 @@ import { UploadComponent } from './pages/upload/upload.component';
     LinkInfoComponent,
     HeadingInfoComponent,
     ResultsetTableComponent,
-    UploadComponent
+    UploadComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -113,9 +142,10 @@ import { UploadComponent } from './pages/upload/upload.component';
     HttpClientModule,
     DxcLinkModule,
     DxcHeadingModule,
-    DxcResultsetTableModule
+    DxcResultsetTableModule,
+    DxcTextareaModule,
   ],
-  providers: [{ provide: 'ThemeService', useClass: ThemeService }],
-  bootstrap: [AppComponent]
+  providers: [{ provide: "ThemeService", useClass: ThemeService }],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ projects/dxc-ngx-cdk-site/src/app/pages/text-area/text-area.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { DxcTableModule, DXCInputTextModule, DxcLinkModule,DxcTagModule, DxcTextareaModule } from '@dxc-technology/halstack-angular';
+import { TabbedSectionModule } from '../../components/tabbed-section/tabbed-section.module';
+import { ExampleViewerModule } from '../../components/example-viewer/example-viewer.module';
+import { TextAreaComponent } from './text-area.component';
+import { ExampleViewerComponent } from '../../components/example-viewer/example-viewer.component';
+import { CodePlaygroundModule } from '../../components/code-playground/code-playground.module';
+import { ComponentsSidenavModule } from '../components-sidenav/components-sidenav.module';
+import { TextAreaApiComponent } from '../../components/examples/text-area/text-area-api/text-area-api.component';
+import { TextAreaPropertiesTableComponent } from '../../components/examples/text-area/properties/text-area-properties-table/text-area-properties-table.component';
+import { TextAreaThemeComponent } from '../../components/examples/text-area/text-area-theme/text-area-theme.component';
+import { TextAreaExampleComponent } from '../../components/examples/text-area/text-area-example/text-area-example.component';
+import { TextAreaDefaultComponent } from '../../components/examples/text-area/text-area-default/text-area-default.component';
+import { TextAreaImportComponent } from '../../components/examples/text-area/text-area-import/text-area-import.component';
+import { TextAreaDisabledComponent } from '../../components/examples/text-area/text-area-disabled/text-area-disabled.component';
+import { TextAreaFillParentComponent } from '../../components/examples/text-area/text-area-fill-parent/text-area-fill-parent.component';
+import { TextAreaInvalidComponent } from '../../components/examples/text-area/text-area-invalid/text-area-invalid.component';
+import { TextAreaRequiredComponent } from '../../components/examples/text-area/text-area-required/text-area-required.component';
+import { TextAreaUncontrolledComponent } from '../../components/examples/text-area/text-area-uncontrolled/text-area-uncontrolled.component';
+
+@NgModule({
+  declarations: [
+    TextAreaComponent,
+    TextAreaApiComponent,
+    TextAreaPropertiesTableComponent,
+    TextAreaThemeComponent,
+    TextAreaExampleComponent,
+    TextAreaDefaultComponent,
+    TextAreaImportComponent,
+    TextAreaDisabledComponent,
+    TextAreaFillParentComponent,
+    TextAreaInvalidComponent,
+    TextAreaRequiredComponent,
+    TextAreaUncontrolledComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    DxcTableModule,
+    TabbedSectionModule,
+    DxcTextareaModule,
+    ExampleViewerModule,
+    DxcLinkModule,
+    DxcTagModule,
+    CodePlaygroundModule,
+    ComponentsSidenavModule
+  ],
+  exports: [
+    TextAreaComponent,
+    TextAreaApiComponent,
+    TextAreaPropertiesTableComponent,
+    TextAreaThemeComponent,
+    TextAreaExampleComponent,
+    TextAreaDefaultComponent,
+    TextAreaImportComponent,
+    TextAreaDisabledComponent,
+    TextAreaFillParentComponent,
+    TextAreaInvalidComponent,
+    TextAreaRequiredComponent,
+    TextAreaUncontrolledComponent,
+  ],
+  entryComponents: [
+    ExampleViewerComponent
+  ]
+})
+export class TextAreaModule { }

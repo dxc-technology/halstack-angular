789cd474fc1def3a6b95c2a874e9ef449a67e5d9
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-09-11 15:18:30 +0200

    Changed chip for status in dropdown

--- projects/dxc-ngx-cdk-site/src/app/pages/dropdown/dropdown.module.ts
+++ projects/dxc-ngx-cdk-site/src/app/pages/dropdown/dropdown.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
-import { DxcTableModule, DXCDropdownModule,DxcTagModule } from "@dxc-technology/halstack-angular";
+import { DxcTableModule, DXCDropdownModule,DxcTagModule, DxcChipModule } from "@dxc-technology/halstack-angular";
 import { TabbedSectionModule } from "src/app/components/tabbed-section/tabbed-section.module";
 import { ExampleViewerModule } from "src/app/components/example-viewer/example-viewer.module";
 import { DropdownTablePropertiesComponent } from "src/app/components/examples/dropdown/properties/dropdown-table-properties.component";
@@ -41,7 +41,8 @@ import { DropdownApiComponent } from '../../components/examples/dropdown/dropdow
     DxcTagModule,
     CodePlaygroundModule,
     ComponentsSidenavModule,
-    ColorPreviewModule
+    ColorPreviewModule,
+    DxcChipModule
   ],
   exports: [
     DropdownTablePropertiesComponent,

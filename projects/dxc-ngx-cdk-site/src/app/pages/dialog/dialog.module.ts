c26f9146ff28a7530aeae547061cbef27e0a1624
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-09-11 15:17:09 +0200

    Changed chip for status in dialog

--- projects/dxc-ngx-cdk-site/src/app/pages/dialog/dialog.module.ts
+++ projects/dxc-ngx-cdk-site/src/app/pages/dialog/dialog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
-import { DxcTableModule, DxcButtonModule, DXCDialogModule,DxcTagModule } from '@dxc-technology/halstack-angular';
+import { DxcTableModule, DxcButtonModule, DXCDialogModule,DxcTagModule, DxcChipModule } from '@dxc-technology/halstack-angular';
 import { TabbedSectionModule} from "src/app/components/tabbed-section/tabbed-section.module";
 import { ExampleViewerModule } from "src/app/components/example-viewer/example-viewer.module";
 import { DialogTablePropertiesComponent } from "src/app/components/examples/dialog/properties/dialog-table-properties.component";
@@ -42,7 +42,8 @@ import { ColorPreviewModule } from '../../components/color-preview/color-preview
     DxcTagModule,
     CodePlaygroundModule,
     ComponentsSidenavModule,
-    ColorPreviewModule
+    ColorPreviewModule,
+    DxcChipModule
   ],
   exports: [
     DialogTablePropertiesComponent,

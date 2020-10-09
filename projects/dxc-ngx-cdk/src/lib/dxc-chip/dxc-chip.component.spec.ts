c945c23e0959e7d264d2ce255bd554cc7def4762
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-10-09 13:23:22 +0200

    Chip: Cleaning + prettier

--- projects/dxc-ngx-cdk/src/lib/dxc-chip/dxc-chip.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-chip/dxc-chip.component.spec.ts
@@ -1,15 +1,12 @@
-import { render, fireEvent } from "@testing-library/angular";
+import { render } from "@testing-library/angular";
 import { DxcChipComponent } from "./dxc-chip.component";
-import { screen } from "@testing-library/dom";
-import { MatCheckboxModule } from "@angular/material";
 
 describe("DxcChip tests", () => {
   test("should render dxc-chip", async () => {
     const { getByText } = await render(DxcChipComponent, {
-      componentProperties: { label: "test-chip" }
+      componentProperties: { label: "test-chip" },
     });
 
     expect(getByText("test-chip"));
   });
-
 });

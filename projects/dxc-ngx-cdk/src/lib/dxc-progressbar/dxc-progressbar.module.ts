bb044480e68e3d5005067ca38ca90e626d534f1b
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-10-09 14:11:51 +0200

    Progressbar: Cleaning + prettier

--- projects/dxc-ngx-cdk/src/lib/dxc-progressbar/dxc-progressbar.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-progressbar/dxc-progressbar.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { MatProgressBarModule } from '@angular/material';
-import { DxcProgressbarComponent } from './dxc-progressbar.component';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { MatProgressBarModule } from "@angular/material";
+import { DxcProgressbarComponent } from "./dxc-progressbar.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [DxcProgressbarComponent],
   imports: [CommonModule, MatProgressBarModule],
-  exports: [DxcProgressbarComponent]
+  exports: [DxcProgressbarComponent],
 })
 export class DxcProgressbarModule {}

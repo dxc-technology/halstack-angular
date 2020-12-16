5c49861723a3e60545f8a792f8ef968a863b86fc
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-12-16 14:52:35 +0100

    Link examples with codesandbox

--- projects/dxc-ngx-cdk-site/src/app/components/examples/link/link-example/link-example.component.ts
+++ projects/dxc-ngx-cdk-site/src/app/components/examples/link/link-example/link-example.component.ts
@@ -1,11 +1,4 @@
-import { Component, OnInit,Inject } from '@angular/core';
-import { Example } from '../../../../model/example';
-import { ExampleService } from 'src/app/service/example.service';
-import { LinkDefaultComponent } from '../link-default/link-default.component';
-import { LinkIconComponent } from '../link-icon/link-icon.component';
-import { LinkDisabledComponent } from '../link-disabled/link-disabled.component';
-import { LinkUndercoratedComponent } from '../link-undercorated/link-undercorated.component';
-import { LinkActionComponent } from '../link-action/link-action.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'link-example',
@@ -14,88 +7,8 @@ import { LinkActionComponent } from '../link-action/link-action.component';
 })
 export class LinkExampleComponent implements OnInit {
 
-  examples: Array<Example>;
+  constructor() {}
 
-  constructor(@Inject("ExampleService") private exampleService: ExampleService) { 
-    this.examples =  new Array();
-  }
-
-  ngOnInit() {
-
-    this.createExamples();
-
-  }
-
-  private createExamples() {
-    this.exampleService
-      .getCodeExample("link/link-default/link-default.component").subscribe(resp1 => {
-        this.examples.push(this.exampleService.generateExample({
-          title: 'Default Link',
-          component: LinkDefaultComponent,
-          selector: "example1",
-          examples: [
-            resp1[0],
-            resp1[1],
-            resp1[2]
-          ]
-        }));
-      });
-
-      this.exampleService
-      .getCodeExample("link/link-undercorated/link-undercorated.component").subscribe(resp1 => {
-        this.examples.push(this.exampleService.generateExample({
-          title: 'Undecorated link with new window',
-          component: LinkUndercoratedComponent,
-          selector: "example2",
-          examples: [
-            resp1[0],
-            resp1[1],
-            resp1[2]
-          ]
-        }));
-      });
-
-      this.exampleService
-      .getCodeExample("link/link-disabled/link-disabled.component").subscribe(resp1 => {
-        this.examples.push(this.exampleService.generateExample({
-          title: 'Disabled Link',
-          component: LinkDisabledComponent,
-          selector: "example3",
-          examples: [
-            resp1[0],
-            resp1[1],
-            resp1[2]
-          ]
-        }));
-      });
-
-      this.exampleService
-      .getCodeExample("link/link-icon/link-icon.component").subscribe(resp1 => {
-        this.examples.push(this.exampleService.generateExample({
-          title: 'Link with icon',
-          component: LinkIconComponent,
-          selector: "example4",
-          examples: [
-            resp1[0],
-            resp1[1],
-            resp1[2]
-          ]
-        }));
-      });
-
-      this.exampleService
-      .getCodeExample("link/link-action/link-action.component").subscribe(resp1 => {
-        this.examples.push(this.exampleService.generateExample({
-          title: 'Link with action',
-          component: LinkActionComponent,
-          selector: "example5",
-          examples: [
-            resp1[0],
-            resp1[1],
-            resp1[2]
-          ]
-        }));
-      });
-    }
+  ngOnInit() {}
 
 }

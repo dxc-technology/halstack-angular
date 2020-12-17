6ec00dabe4a71bb564fa8627e45f1d960efff6c1
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2020-12-17 14:17:48 +0100

    Replacing dynamic module examples by codesandbox in autocomplete

--- projects/dxc-ngx-cdk-site/src/app/pages/autocomplete/autocomplete.module.ts
+++ projects/dxc-ngx-cdk-site/src/app/pages/autocomplete/autocomplete.module.ts
@@ -13,11 +13,7 @@ import { TabbedSectionModule } from "../../components/tabbed-section/tabbed-sect
 import { ExampleViewerModule } from "../../components/example-viewer/example-viewer.module";
 import { AutocompleteComponent } from "./autocomplete.component";
 import { ExampleViewerComponent } from "../../components/example-viewer/example-viewer.component";
-import { AutocompleteAsyncControlledComponent } from '../../components/examples/autocomplete/autocomplete-asynchronous-controlled/autocomplete-asynchronous-controlled.component';
-import { AutocompleteAsyncUncontrolledComponent } from '../../components/examples/autocomplete/autocomplete-asynchronous-uncontrolled/autocomplete-asynchronous-uncontrolled.component';
 import { AutocompleteExampleComponent } from "../../components/examples/autocomplete/autocomplete-example/autocomplete-example.component";
-import { AutocompleteSyncControlledComponent } from "../../components/examples/autocomplete/autocomplete-synchronous-controlled/autocomplete-synchronous-controlled.component";
-import { AutocompleteSyncUncontrolledComponent } from "../../components/examples/autocomplete/autocomplete-synchronous-uncontrolled/autocomplete-synchronous-uncontrolled.component";
 import { AutocompleteUsageComponent } from "../../components/examples/autocomplete/autocomplete-usage/autocomplete-usage.component";
 import { CodePlaygroundModule } from '../../components/code-playground/code-playground.module';
 import { ComponentsSidenavModule } from '../components-sidenav/components-sidenav.module';
@@ -27,11 +23,7 @@ import { ColorPreviewModule } from '../../components/color-preview/color-preview
 @NgModule({
   declarations: [
     AutocompleteComponent,
-    AutocompleteAsyncControlledComponent,
-    AutocompleteAsyncUncontrolledComponent,
     AutocompleteExampleComponent,
-    AutocompleteSyncControlledComponent,
-    AutocompleteSyncUncontrolledComponent,
     AutocompleteUsageComponent,
     AutocompleteThemeComponent
   ],
@@ -52,14 +44,10 @@ import { ColorPreviewModule } from '../../components/color-preview/color-preview
   ],
   exports: [
     AutocompleteComponent,
-    AutocompleteAsyncControlledComponent,
-    AutocompleteAsyncUncontrolledComponent,
     AutocompleteExampleComponent,
-    AutocompleteSyncControlledComponent,
-    AutocompleteSyncUncontrolledComponent,
     AutocompleteUsageComponent,
     AutocompleteThemeComponent
   ],
   entryComponents: [ExampleViewerComponent]
 })
-export class AutocompleteModule {}
+export class AutocompleteModule { }

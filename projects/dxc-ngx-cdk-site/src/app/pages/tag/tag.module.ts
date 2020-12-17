ec81902f85fc40b5c90d4dd1cb7c830f039d1a15
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-12-17 13:15:45 +0100

    Tag examples with codesandbox

--- projects/dxc-ngx-cdk-site/src/app/pages/tag/tag.module.ts
+++ projects/dxc-ngx-cdk-site/src/app/pages/tag/tag.module.ts
@@ -3,14 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { DxcTableModule, DxcTagModule, DxcChipModule,DxcHeadingModule } from '@dxc-technology/halstack-angular';
 import { TabbedSectionModule } from '../../components/tabbed-section/tabbed-section.module';
-import { ExampleViewerModule } from '../../components/example-viewer/example-viewer.module';
-import { ExampleViewerComponent } from '../../components/example-viewer/example-viewer.component';
 import { TagComponent } from './tag.component';
 import { TagTablePropertiesComponent } from '../../components/examples/tag/properties/tag-table-properties/tag-table-properties.component';
-import { TagSizedComponent } from 'src/app/components/examples/tag/tag-sized/tag-sized.component';
-import { TagActionComponent } from 'src/app/components/examples/tag/tag-action/tag-action.component';
-import { TagLinkComponent } from 'src/app/components/examples/tag/tag-link/tag-link.component';
-import { TagDefaultComponent } from 'src/app/components/examples/tag/tag-default/tag-default.component';
 import { TagExampleComponent } from 'src/app/components/examples/tag/tag-example/tag-example.component';
 import { TagImportComponent } from '../../components/examples/tag/tag-import/tag-import.component';
 import { CodePlaygroundModule } from '../../components/code-playground/code-playground.module';
@@ -22,10 +16,6 @@ import { TagThemeComponent } from '../../components/examples/tag/tag-theme/tag-t
   declarations: [
     TagComponent,
     TagExampleComponent,
-    TagDefaultComponent,
-    TagLinkComponent,
-    TagActionComponent,
-    TagSizedComponent,
     TagTablePropertiesComponent,
     TagImportComponent,
     TagApiComponent,
@@ -37,7 +27,6 @@ import { TagThemeComponent } from '../../components/examples/tag/tag-theme/tag-t
     DxcTableModule,
     TabbedSectionModule,
     DxcTagModule,
-    ExampleViewerModule,
     CodePlaygroundModule,
     ComponentsSidenavModule,
     DxcChipModule,
@@ -46,17 +35,10 @@ import { TagThemeComponent } from '../../components/examples/tag/tag-theme/tag-t
   exports: [
     TagComponent,
     TagExampleComponent,
-    TagDefaultComponent,
-    TagLinkComponent,
-    TagActionComponent,
-    TagSizedComponent,
     TagTablePropertiesComponent,
     TagImportComponent,
     TagApiComponent,
     TagThemeComponent
-  ],
-  entryComponents: [
-    ExampleViewerComponent
   ]
 })
 export class TagModule { }

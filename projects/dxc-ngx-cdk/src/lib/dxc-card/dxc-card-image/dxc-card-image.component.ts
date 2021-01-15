07e8ba571ad1670f6850bd68d7a3e56e3a766ba8
Jiale <jialestrabajos@gmail.com>
2021-01-15 13:02:08 +0100

    adding DxcCardImageComponent

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-card/dxc-card-image/dxc-card-image.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "dxc-card-image",
+  templateUrl: "./dxc-card-image.component.html",
+})
+export class DxcCardImageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- projects/dxc-ngx-cdk/src/lib/dxc-card/dxc-card.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-card/dxc-card.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { DxcCardComponent } from "./dxc-card.component";
 import { CommonModule } from "@angular/common";
+import { DxcCardImageComponent } from './dxc-card-image/dxc-card-image.component';
 
 @NgModule({
-  declarations: [DxcCardComponent],
+  declarations: [DxcCardComponent, DxcCardImageComponent],
   imports: [CommonModule, MatCardModule],
-  exports: [DxcCardComponent],
+  exports: [DxcCardComponent, DxcCardImageComponent],
 })
 export class DxcCardModule {}

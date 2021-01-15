93479539eb101663c5c4bcc4197fd212bff98cf5
agonzalez97 <aidamaria.gonzalez@dxc.com>
2021-01-15 14:01:34 +0100

    Added chip prefix and suffix icon component

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-chip/dxc-chip-prefix-icon/dxc-chip-prefix-icon.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component,
+} from "@angular/core";
+
+@Component({
+  selector: "dxc-chip-prefix-icon",
+  templateUrl: "./dxc-chip-prefix-icon.component.html"
+})
+export class DxcChipPrefixIconComponent {
+
+  constructor() {}
+
+}
---
+++ projects/dxc-ngx-cdk/src/lib/dxc-chip/dxc-chip-suffix-icon/dxc-chip-suffix-icon.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component,
+} from "@angular/core";
+
+@Component({
+  selector: "dxc-chip-suffix-icon",
+  templateUrl: "./dxc-chip-suffix-icon.component.html"
+})
+export class DxcChipSuffixIconComponent {
+
+  constructor() {}
+
+}

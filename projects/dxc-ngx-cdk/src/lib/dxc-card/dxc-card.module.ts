580fc168878391b77324ee29b7c3d67f422245cf
Jiale <jialestrabajos@gmail.com>
2021-01-21 15:51:17 +0100

    doc improvement and less constraints for icons

--- projects/dxc-ngx-cdk/src/lib/dxc-card/dxc-card.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-card/dxc-card.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { DxcCardComponent } from "./dxc-card.component";
 import { CommonModule } from "@angular/common";
-import { DxcCardImageComponent } from './dxc-card-image/dxc-card-image.component';
 
 @NgModule({
-  declarations: [DxcCardComponent, DxcCardImageComponent],
+  declarations: [DxcCardComponent],
   imports: [CommonModule, MatCardModule],
-  exports: [DxcCardComponent, DxcCardImageComponent],
+  exports: [DxcCardComponent],
 })
 export class DxcCardModule {}

89b66b580cb45168b0230bb2a8874c90a19c5b06
agonzalez97 <aidamaria.gonzalez@dxc.com>
2021-02-11 17:06:36 +0100

    Added tests accordion group

--- projects/dxc-ngx-cdk/src/lib/dxc-accordionGroup/dxc-accordionGroup.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-accordionGroup/dxc-accordionGroup.component.spec.ts
@@ -7,16 +7,61 @@ import { DxcAccordionModule } from '../dxc-accordion/dxc-accordion.module';
 import { DxcAccordionGroupModule } from './dxc-accordionGroup.module';
 
 describe("DxcAccordion tests", () => {
-  test("should render dxc-accordion-group", async () => {
+  test("should render dxc-accordion-group uncontrolled", async () => {
+    const projection1 = "Lorem ipsum dolor sit amet";
+    const projection2 = "Consectetur adipiscing elit";
+    const projection3 = "Maecenas enim ipsum";
+    const accordions = await render(DxcAccordionGroupComponent, {
+      template: `<dxc-accordion-group>
+                  <dxc-accordion label="test-accordion1">${projection1}</dxc-accordion>
+                  <dxc-accordion label="test-accordion2">${projection2}</dxc-accordion>
+                  <dxc-accordion label="test-accordion3">${projection3}</dxc-accordion>
+                </dxc-accordion-group>`,
+      imports: [DxcAccordionGroupModule,DxcAccordionModule,MatExpansionModule],
+      excludeComponentDeclaration: true
+    });
+    accordions.detectChanges();
+
+    expect(accordions.getByText("test-accordion1")).toBeTruthy();
+    expect(accordions.getByText("test-accordion2")).toBeTruthy();
+    expect(accordions.getByText("test-accordion3")).toBeTruthy();
+  });
+
+  test("Accordion Group Uncontrolled with undefined ActiveIndex", async () => {
+    const projection1 = "Lorem ipsum dolor sit amet";
+    const projection2 = "Consectetur adipiscing elit";
+    const projection3 = "Maecenas enim ipsum";
+    const accordions = await render(DxcAccordionGroupComponent, {
+      template: `<dxc-accordion-group [indexActive]="undefined">
+                  <dxc-accordion label="test-accordion1">${projection1}</dxc-accordion>
+                  <dxc-accordion label="test-accordion2">${projection2}</dxc-accordion>
+                  <dxc-accordion label="test-accordion3">${projection3}</dxc-accordion>
+                </dxc-accordion-group>`,
+      imports: [DxcAccordionGroupModule,DxcAccordionModule,MatExpansionModule],
+      excludeComponentDeclaration: true
+    });
+    accordions.detectChanges();
+
+    expect(accordions.getByText("test-accordion1")).toBeTruthy();
+    expect(accordions.getByText("test-accordion2")).toBeTruthy();
+    expect(accordions.getByText("test-accordion3")).toBeTruthy();
+
+    expect(accordions.getByText(projection1).hidden);
+    expect(accordions.getByText(projection2).hidden);
+    expect(accordions.getByText(projection3).hidden);
+
+  });
+
+  test("Accordion Group uncontrolled with function", async () => {
     const projection1 = "Lorem ipsum dolor sit amet";
     const projection2 = "Consectetur adipiscing elit";
     const projection3 = "Maecenas enim ipsum";
     const onClickFunction = jest.fn();
     const accordions = await render(DxcAccordionGroupComponent, {
-      template: `<dxc-accordion-group>
-                  <dxc-accordion label="test-accordion 1" (onClick)="onClickFunction($event)">${projection1}</dxc-accordion>
-                  <dxc-accordion label="test-accordion 2" (onClick)="onClickFunction($event)">${projection2}</dxc-accordion>
-                  <dxc-accordion label="test-accordion 3" (onClick)="onClickFunction($event)">${projection3}</dxc-accordion>
+      template: `<dxc-accordion-group (onActiveChange)="onClickFunction($event)">
+                  <dxc-accordion label="test-accordion1">${projection1}</dxc-accordion>
+                  <dxc-accordion label="test-accordion2">${projection2}</dxc-accordion>
+                  <dxc-accordion label="test-accordion3">${projection3}</dxc-accordion>
                 </dxc-accordion-group>`,
       componentProperties: { onClickFunction },
       imports: [DxcAccordionGroupModule,DxcAccordionModule,MatExpansionModule],
@@ -24,21 +69,38 @@ describe("DxcAccordion tests", () => {
     });
     accordions.detectChanges();
 
-    expect(accordions.getByText("test-accordion 1")).toBeTruthy();
-    expect(accordions.getByText("test-accordion 2")).toBeTruthy();
-    expect(accordions.getByText("test-accordion 3")).toBeTruthy();
+    expect(accordions.getByText("test-accordion1")).toBeTruthy();
+    expect(accordions.getByText("test-accordion2")).toBeTruthy();
+    expect(accordions.getByText("test-accordion3")).toBeTruthy();
+
+    expect(accordions.getByText(projection1).hidden);
+    expect(accordions.getByText(projection2).hidden);
+    expect(accordions.getByText(projection3).hidden);
+    fireEvent.click(accordions.getByText("test-accordion1"));
+    expect(onClickFunction).toHaveBeenCalled();
+    expect(screen.getByText(projection1).hidden).toBeFalsy();
+    expect(accordions.getByText(projection2).hidden);
+    expect(accordions.getByText(projection3).hidden);
+    fireEvent.click(accordions.getByText("test-accordion2"));
+    expect(onClickFunction).toHaveBeenCalled();
+    expect(screen.getByText(projection2).hidden).toBeFalsy();
+    expect(accordions.getByText(projection1).hidden);
+    expect(accordions.getByText(projection3).hidden);
+    fireEvent.click(accordions.getByText("test-accordion3"));
+    expect(onClickFunction).toHaveBeenCalled();
+    expect(screen.getByText(projection3).hidden).toBeFalsy();
+    expect(accordions.getByText(projection1).hidden);
+    expect(accordions.getByText(projection2).hidden);
   });
 
-  test("should hide not selected dxc-accordions", async () => {
+  test("Accordion Group Controlled with static index active", async () => {
     const projection1 = "Lorem ipsum dolor sit amet";
     const projection2 = "Consectetur adipiscing elit";
-    const projection3 = "Maecenas enim ipsum";
     const onClickFunction = jest.fn();
     const accordions = await render(DxcAccordionGroupComponent, {
-      template: `<dxc-accordion-group>
-                  <dxc-accordion label="test-accordion 1" (onClick)="onClickFunction($event)">${projection1}</dxc-accordion>
-                  <dxc-accordion label="test-accordion 2" (onClick)="onClickFunction($event)">${projection2}</dxc-accordion>
-                  <dxc-accordion label="test-accordion 3" (onClick)="onClickFunction($event)">${projection3}</dxc-accordion>
+      template: `<dxc-accordion-group [indexActive]="0">
+                  <dxc-accordion label="test-accordion1">${projection1}</dxc-accordion>
+                  <dxc-accordion label="test-accordion2">${projection2}</dxc-accordion>
                 </dxc-accordion-group>`,
       componentProperties: { onClickFunction },
       imports: [DxcAccordionGroupModule,DxcAccordionModule,MatExpansionModule],
@@ -46,30 +108,35 @@ describe("DxcAccordion tests", () => {
     });
     accordions.detectChanges();
 
-    expect(accordions.getByText("test-accordion 1")).toBeTruthy();
-    expect(accordions.getByText("test-accordion 2")).toBeTruthy();
-    expect(accordions.getByText("test-accordion 3")).toBeTruthy();
+    expect(accordions.getByText("test-accordion1")).toBeTruthy();
+    expect(accordions.getByText("test-accordion2")).toBeTruthy();
 
-    expect(accordions.getByText(projection1).hidden);
+    expect(accordions.getByText(projection1)).toBeTruthy();
     expect(accordions.getByText(projection2).hidden);
-    expect(accordions.getByText(projection3).hidden);
 
-    fireEvent.click(accordions.getByText("test-accordion 1"));
-    expect(onClickFunction).toHaveBeenCalled();
-    expect(accordions.getByText(projection1).hidden).toBeFalsy();
-    expect(accordions.getByText(projection2).hidden);
-    expect(accordions.getByText(projection3).hidden);
-
-    fireEvent.click(accordions.getByText("test-accordion 2"));
-    expect(onClickFunction).toHaveBeenCalled();
-    expect(accordions.getByText(projection1).hidden);
-    expect(accordions.getByText(projection2).hidden).toBeFalsy();
-    expect(accordions.getByText(projection3).hidden);
-
-    fireEvent.click(accordions.getByText("test-accordion 3"));
-    expect(onClickFunction).toHaveBeenCalled();
-    expect(accordions.getByText(projection1).hidden);
-    expect(accordions.getByText(projection2).hidden);
-    expect(accordions.getByText(projection3).hidden).toBeFalsy();
   });
+
+  test("Accordion Group Controlled with static index active and function", async () => {
+    const projection1 = "Lorem ipsum dolor sit amet";
+    const projection2 = "Consectetur adipiscing elit";
+    const onClickFunction = jest.fn();
+    const accordions = await render(DxcAccordionGroupComponent, {
+      template: `<dxc-accordion-group  [indexActive]="null" (onActiveChange)="onClickFunction($event)">
+                  <dxc-accordion label="test-accordion1">${projection1}</dxc-accordion>
+                  <dxc-accordion label="test-accordion2">${projection2}</dxc-accordion>
+                </dxc-accordion-group>`,
+      componentProperties: { onClickFunction },
+      imports: [DxcAccordionGroupModule,DxcAccordionModule,MatExpansionModule],
+      excludeComponentDeclaration: true
+    });
+    accordions.detectChanges();
+
+    expect(accordions.getByText("test-accordion1")).toBeTruthy();
+    expect(accordions.getByText("test-accordion2")).toBeTruthy();
+
+    expect(accordions.getByText(projection1).hidden);
+    expect(accordions.getByText(projection2).hidden);
+
+  });
+
 });

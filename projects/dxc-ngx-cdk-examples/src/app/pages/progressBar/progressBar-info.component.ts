ff82b2488f4b0559ec49893f5db93392741020b1
agonzalez97 <aidamaria.gonzalez@dxc.com>
2021-03-15 18:06:30 +0100

    Added some examples in examples page

--- projects/dxc-ngx-cdk-examples/src/app/pages/progressBar/progressBar-info.component.ts
+++ projects/dxc-ngx-cdk-examples/src/app/pages/progressBar/progressBar-info.component.ts
@@ -3,8 +3,18 @@ import { Component } from "@angular/core";
 @Component({
   selector: "progressBar-info",
   templateUrl: "./progressBar-info.component.html",
-  styleUrls: ["./progressBar-info.component.scss"]
+  styleUrls: ["./progressBar-info.component.scss"],
 })
 export class ProgressInfoComponent {
   constructor() {}
+
+  isVisible = false;
+
+  showProgressbar() {
+    this.isVisible = !this.isVisible;
+
+    setTimeout(() => {
+      this.isVisible = false;
+    }, 3000);
+  }
 }

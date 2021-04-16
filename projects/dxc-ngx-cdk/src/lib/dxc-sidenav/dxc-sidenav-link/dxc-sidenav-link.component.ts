7b4aeb149da55f6bc5d3012fd864df9be5bce13a
agonzalez97 <aidamaria.gonzalez@dxc.com>
2021-04-16 15:17:56 +0200

    [Minor] Added tabIndexValue in sidenav component for links

--- projects/dxc-ngx-cdk/src/lib/dxc-sidenav/dxc-sidenav-link/dxc-sidenav-link.component.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-sidenav/dxc-sidenav-link/dxc-sidenav-link.component.ts
@@ -7,6 +7,7 @@ import {
   Output,
 } from "@angular/core";
 import { CssUtils } from "../../utils";
+import { coerceNumberProperty } from '@angular/cdk/coercion';
 @Component({
   selector: "dxc-sidenav-link",
   templateUrl: "./dxc-sidenav-link.component.html",
@@ -14,6 +15,14 @@ import { CssUtils } from "../../utils";
 })
 export class DxcSidenavLinkComponent implements OnInit {
   @Input() href: string;
+  @Input()
+  get tabIndexValue(): number {
+    return this._tabIndexValue;
+  }
+  set tabIndexValue(value: number) {
+    this._tabIndexValue = coerceNumberProperty(value);
+  }
+  private _tabIndexValue;
   @Output() onClick = new EventEmitter<any>();
 
   constructor(public element: ElementRef) {}

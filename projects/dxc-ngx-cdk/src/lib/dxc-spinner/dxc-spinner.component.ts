f6fbd6569c54a3d5fd97da92d1ec41ff9f0d41fc
agonzalez97 <aidamaria.gonzalez@dxc.com>
2021-10-19 17:49:14 +0200

    New implementation spinner
    Removed material from spinner component

--- projects/dxc-ngx-cdk/src/lib/dxc-spinner/dxc-spinner.component.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-spinner/dxc-spinner.component.ts
@@ -1,25 +1,27 @@
 import {
   Component,
-  OnInit,
   Input,
   HostBinding,
   SimpleChanges,
+  ElementRef,
+  ViewChild,
+  OnInit,
 } from "@angular/core";
-import { css } from "emotion";
 import { BehaviorSubject } from "rxjs";
+import { css } from "emotion";
 import { CssUtils } from "../utils";
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { coerceBooleanProperty } from "@angular/cdk/coercion";
 
 @Component({
   selector: "dxc-spinner",
   templateUrl: "./dxc-spinner.component.html",
-  styleUrls: ["./dxc-spinner.component.scss"],
   providers: [CssUtils],
 })
-export class DxcSpinnerComponent {
-  type: string = "indeterminate";
+export class DxcSpinnerComponent implements OnInit {
   @Input() value: number;
   @Input() label: string;
+  @Input() mode: string = "large";
+  @Input() margin: any;
   @Input()
   get showValue(): boolean {
     return this._showValue;
@@ -28,18 +30,34 @@ export class DxcSpinnerComponent {
     this._showValue = coerceBooleanProperty(value);
   }
   private _showValue = false;
-  @Input() mode: string = "large";
-  @Input() margin: any;
 
-  @HostBinding("class") className;
-
+  @HostBinding("class") className: string;
   @HostBinding("class.overlay") isOverlayed: boolean = false;
   @HostBinding("class.small") isSmall: boolean = false;
   @HostBinding("class.large") isLarge: boolean = true;
 
+  @ViewChild("svgBackgroundLarge") svgBackgroundLarge: ElementRef;
+  @ViewChild("svgBackgroundSmall") svgBackgroundSmall: ElementRef;
+  @ViewChild("svgLarge") svgLarge: ElementRef;
+  @ViewChild("svgSmall") svgSmall: ElementRef;
+  @ViewChild("circleBackgroundLarge") circleBackgroundLarge: ElementRef;
+  @ViewChild("circleBackgroundSmall") circleBackgroundSmall: ElementRef;
+  @ViewChild("circleLarge") circleLarge: ElementRef;
+  @ViewChild("circleSmall") circleSmall: ElementRef;
+
+  isIndeterminate: boolean = true;
+
+  largeSize: number = 140;
+  smallSize: number = 16;
+  radioLargeSize: number = 65;
+  radioSmallSize: number = 6;
+
   defaultInputs = new BehaviorSubject<any>({
     showValue: false,
-    mode: "large"
+    mode: "large",
+    value: 0,
+    label: null,
+    margin: null,
   });
 
   constructor(private utils: CssUtils) {}
@@ -47,21 +65,21 @@ export class DxcSpinnerComponent {
   public ngOnChanges(changes: SimpleChanges): void {
     if (this.value || this.value === 0) {
       if (this.value <= 100 && this.value >= 0) {
-        this.type = "determinate";
+        this.isIndeterminate = false;
       } else {
         if (this.value > 100) {
-          this.type = "determinate";
+          this.isIndeterminate = false;
           this.value = 100;
         } else if (this.value < 0) {
-          this.type = "determinate";
+          this.isIndeterminate = false;
           this.value = 0;
         } else {
           this.value = undefined;
-          this.type = "indeterminate";
+          this.isIndeterminate = true;
         }
       }
     } else {
-      this.type = "indeterminate";
+      this.isIndeterminate = true;
     }
     if (this.mode === "overlay") {
       this.isOverlayed = true;
@@ -87,13 +105,303 @@ export class DxcSpinnerComponent {
   public ngOnInit(): void {
     this.className = `${this.getDynamicStyle(this.defaultInputs.getValue())}`;
     if (this.value) {
-      this.type = "determinate";
+      this.isIndeterminate = false;
     }
+  }
+
+  ngAfterViewInit() {
+    if (this.mode !== "small") {
+      this.svgBackgroundLarge.nativeElement.setAttribute(
+        "viewBox",
+        "0 0 " + this.largeSize + " " + this.largeSize
+      );
+      this.svgLarge.nativeElement.setAttribute(
+        "viewBox",
+        "0 0 " + this.largeSize + " " + this.largeSize
+      );
+      this.circleBackgroundLarge.nativeElement.setAttribute(
+        "cx",
+        this.largeSize / 2
+      );
+      this.circleBackgroundLarge.nativeElement.setAttribute(
+        "cy",
+        this.largeSize / 2
+      );
+      this.circleLarge.nativeElement.setAttribute("cx", this.largeSize / 2);
+      this.circleLarge.nativeElement.setAttribute("cy", this.largeSize / 2);
+      this.circleBackgroundLarge.nativeElement.setAttribute(
+        "r",
+        this.radioLargeSize
+      );
+      this.circleLarge.nativeElement.setAttribute("r", this.radioLargeSize);
+    } else {
+      this.svgBackgroundSmall.nativeElement.setAttribute(
+        "viewBox",
+        "0 0 " + this.smallSize + " " + this.smallSize
+      );
+      this.svgSmall.nativeElement.setAttribute(
+        "viewBox",
+        "0 0 " + this.smallSize + " " + this.smallSize
+      );
+      this.circleBackgroundSmall.nativeElement.setAttribute(
+        "cx",
+        this.smallSize / 2
+      );
+      this.circleBackgroundSmall.nativeElement.setAttribute(
+        "cy",
+        this.smallSize / 2
+      );
+      this.circleSmall.nativeElement.setAttribute("cx", this.smallSize / 2);
+      this.circleSmall.nativeElement.setAttribute("cy", this.smallSize / 2);
+      this.circleBackgroundSmall.nativeElement.setAttribute(
+        "r",
+        this.radioSmallSize
+      );
+      this.circleSmall.nativeElement.setAttribute("r", this.radioSmallSize);
+    }
+  }
+
+  calculateValue(strokeDashArray) {
+    let val = 0;
+    if (this.value >= 0 && this.value <= 100) {
+      val = strokeDashArray * (1 - this.value / 100);
+    }
+    return val;
+  }
+
+  getSpinnerStyle() {
+    return css`
+      position: relative;
+      z-index: 2;
+      svg {
+        transform: rotate(-90deg);
+        top: 0;
+        left: 0;
+        transform-origin: center;
+        overflow: visible;
+      }
+      circle {
+        fill: transparent;
+        stroke-linecap: initial;
+        vector-effect: non-scaling-stroke;
+      }
+    `;
+  }
+
+  getIndeterminateStyle(inputs, size, strokeWidth, strokeDashArray) {
+    return css`
+      .spinnerIndeterminate {
+        ${this.getSpinnerStyle()}
+        height: ${size};
+        width: ${size};
+        svg {
+          animation: 1.4s linear infinite both spinner-svg;
+        }
+        circle {
+          ${inputs.mode !== "small"
+            ? "animation: 1.4s ease-in-out infinite both svg-circle-large;"
+            : "animation: 1.4s ease-in-out infinite both svg-circle-small;"}
+          ${inputs.mode === "overlay"
+            ? "stroke: var(--spinner-overlayTrackCircleColor);"
+            : "stroke: var(--spinner-trackCircleColor);"}
+          stroke-dasharray: ${strokeDashArray};
+          stroke-width: ${strokeWidth}px;
+          transform-origin: 50% 50%;
+        }
+      }
+    `;
+  }
+
+  getDeterminateStyle(inputs, size, strokeWidth, strokeDashArray) {
+    return css`
+      .spinnerDeterminate {
+        ${this.getSpinnerStyle()}
+        height: ${size};
+        width: ${size};
+        circle {
+          animation: none;
+          ${inputs.mode === "overlay"
+            ? "stroke: var(--spinner-overlayTrackCircleColor);"
+            : "stroke: var(--spinner-trackCircleColor);"}
+          stroke-width: ${strokeWidth}px;
+          stroke-dashoffset: ${inputs.mode !== "small"
+            ? this.calculateValue(408)
+            : this.calculateValue(38)};
+          stroke-dasharray: ${strokeDashArray};
+        }
+      }
+    `;
+  }
+
+  getBackgroundCircleStyle(size, strokeWidth, strokeDashArray) {
+    return css`
+      .backgroundSpinner {
+        height: ${size};
+        width: ${size};
+        position: absolute;
+        z-index: 1;
+        circle {
+          animation: none;
+          fill: transparent;
+          stroke: var(--spinner-totalCircleColor);
+          stroke-dasharray: ${strokeDashArray};
+          stroke-linecap: initial;
+          stroke-width: ${strokeWidth}px;
+          transform-origin: 50% 50%;
+          vector-effect: non-scaling-stroke;
+        }
+      }
+    `;
   }
 
   getDynamicStyle(inputs) {
     return css`
       ${this.utils.getMargins(inputs.margin)}
+      display: inline-block;
+      &.overlay {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        position: fixed;
+        top: 0;
+        left: 0;
+        justify-content: center;
+        align-items: center;
+        z-index: 1000;
+      }
+      .spinnerContainer {
+        align-items: center;
+        display: flex;
+        height: inherit;
+        justify-content: center;
+        position: relative;
+        width: inherit;
+        .backOverlay {
+          width: 100vw;
+          height: 100vh;
+          opacity: 1;
+          transition: opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+          position: fixed;
+          top: 0;
+          left: 0;
+          background-color: var(--spinner-overlayBackgroundColor);
+          opacity: var(--spinner-overlayOpacity);
+        }
+        &.overlayed {
+          .labelsContainer {
+            p {
+              &.label {
+                text-align: var(--spinner-overlayLabelTextAlign);
+                font-family: var(--spinner-overlayLabelFontFamily);
+                font-size: var(--spinner-overlayLabelFontSize);
+                font-weight: var(--spinner-overlayLabelFontWeight);
+                font-style: var(--spinner-overlayLabelFontStyle);
+                color: var(--spinner-overlayLabelFontColor);
+              }
+              &.value {
+                text-align: var(--spinner-overlayProgressValueTextAlign);
+                font-family: var(--spinner-overlayProgressValueFontFamily);
+                font-size: var(--spinner-overlayProgressValueFontSize);
+                font-weight: var(--spinner-overlayProgressValueFontWeight);
+                font-style: var(--spinner-overlayProgressValueFontStyle);
+                color: var(--spinner-overlayProgressValueFontColor);
+              }
+            }
+          }
+        }
+        &.overlayed,
+        &.larger {
+          ${this.getBackgroundCircleStyle("140px", 8.5, 408)}
+          ${this.getIndeterminateStyle(inputs, "140px", 8.5, 408)}
+          ${this.getDeterminateStyle(inputs, "140px", 8.5, 408)}
+        }
+        &.smaller {
+          ${this.getBackgroundCircleStyle("16px", 2, 38)}
+          ${this.getIndeterminateStyle(inputs, "16px", 2, 38)}
+          ${this.getDeterminateStyle(inputs, "16px", 2, 38)}
+        }
+        .labelsContainer {
+          display: block;
+          margin: 0 auto;
+          position: absolute;
+          text-align: center;
+          width: 110px;
+          p {
+            margin: 0;
+            &.label,
+            &.value {
+              width: 100%;
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+            &.label {
+              text-align: var(--spinner-labelTextAlign);
+              font-family: var(--spinner-labelFontFamily);
+              font-size: var(--spinner-labelFontSize);
+              font-weight: var(--spinner-labelFontWeight);
+              font-style: var(--spinner-labelFontStyle);
+              color: var(--spinner-labelFontColor);
+            }
+            &.value {
+              text-align: var(--spinner-progressValueTextAlign);
+              font-family: var(--spinner-progressValueFontFamily);
+              font-size: var(--spinner-progressValueFontSize);
+              font-weight: var(--spinner-progressValueFontWeight);
+              font-style: var(--spinner-progressValueFontStyle);
+              color: var(--spinner-progressValueFontColor);
+            }
+          }
+          /* &.notOverlayed {
+            height: auto;
+            width: auto;
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: 1;
+          } */
+        }
+        @keyframes spinner-svg {
+          0% {
+            transform: rotateZ(0deg);
+          }
+          100% {
+            transform: rotateZ(360deg);
+          }
+        }
+        @keyframes svg-circle-large {
+          0% {
+            stroke-dashoffset: 400;
+            transform: rotate(0);
+          }
+
+          50% {
+            stroke-dashoffset: 75;
+            transform: rotate(45deg);
+          }
+
+          100% {
+            stroke-dashoffset: 400;
+            transform: rotate(360deg);
+          }
+        }
+        @keyframes svg-circle-small {
+          0% {
+            stroke-dashoffset: 35;
+            transform: rotate(0);
+          }
+
+          50% {
+            stroke-dashoffset: 8;
+            transform: rotate(45deg);
+          }
+
+          100% {
+            stroke-dashoffset: 35;
+            transform: rotate(360deg);
+          }
+        }
+      }
     `;
   }
 }
--- projects/dxc-ngx-cdk/src/lib/dxc-spinner/dxc-spinner.module.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-spinner/dxc-spinner.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from "@angular/core";
-import { MatIconModule } from "@angular/material/icon";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { DxcSpinnerComponent } from "./dxc-spinner.component";
 import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [DxcSpinnerComponent],
-  imports: [CommonModule, MatProgressSpinnerModule, MatIconModule],
+  imports: [CommonModule],
   exports: [DxcSpinnerComponent],
 })
 export class DxcSpinnerModule {}

95d6413f6d1ebf084fb5d380127ba2e9f6ee3be2
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2021-11-08 15:23:16 +0100

    Updating file format directive

--- projects/dxc-ngx-cdk/src/lib/dxc-file-input/directives/file-format.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FileFormatDirective } from './file-format.directive';
-
-describe('FileFormatDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileFormatDirective();
-    expect(directive).toBeTruthy();
-  });
-});

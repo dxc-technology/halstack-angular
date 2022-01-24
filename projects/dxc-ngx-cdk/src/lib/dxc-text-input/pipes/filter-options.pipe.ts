b9acb469ca3cbb7520d588fa3b39f0f84f3d5782
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2022-01-24 15:50:49 +0100

    In case of value in autosuggest is undefined, optional chaining in filtering

--- projects/dxc-ngx-cdk/src/lib/dxc-text-input/pipes/filter-options.pipe.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-text-input/pipes/filter-options.pipe.ts
@@ -3,11 +3,11 @@ import { DxcTextInputService } from "../services/dxc-text-input.service";
 
 @Pipe({ name: "filterOptions", pure: true })
 export class FilterOptionsPipe implements PipeTransform {
-  constructor(private service: DxcTextInputService){}
+  constructor(private service: DxcTextInputService) {}
 
   public transform(options: Array<any>, actualValue: string): Array<any> {
-    let value = actualValue.toLowerCase();
-    let array = options.filter((element) =>
+    const value = actualValue?.toLowerCase();
+    const array = options?.filter((element) =>
       element.toLowerCase().startsWith(value)
     );
     this.service.setFilteredOptions(array);

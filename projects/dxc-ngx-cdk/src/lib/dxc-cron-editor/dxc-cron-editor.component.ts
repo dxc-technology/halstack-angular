7b58b0d8c52a2af2a6283f0a35bc5ce966b4cb22
Shivam Anand <shivam.anand@dxc.com>
2022-02-15 16:57:18 +0530

    feature/RMA-93690: checkin on behalf vishal

--- projects/dxc-ngx-cdk/src/lib/dxc-cron-editor/dxc-cron-editor.component.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-cron-editor/dxc-cron-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, OnInit, EventEmitter, forwardRef } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter, forwardRef, OnChanges, SimpleChanges } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import { Days, MonthWeeks, Months } from './enums';
 import { ControlValueAccessor, FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
@@ -18,11 +18,27 @@ export const CRON_VALUE_ACCESSOR: any = {
   styleUrls: ['./dxc-cron-editor.component.scss'],
   providers: [CRON_VALUE_ACCESSOR]
 })
-export class DxcCronEditorComponent implements OnInit, ControlValueAccessor {
+export class DxcCronEditorComponent implements OnInit, OnChanges, ControlValueAccessor {
   @Input() public backgroundColor: ThemePalette;
   @Input() public color: ThemePalette;
   @Input() public disabled: boolean;
   @Input() public options: CronOptions;
+  @Input() public cronType:'expression' | 'cust' = 'expression';
+  @Output() public cronTypeChange = new EventEmitter<string>();
+  @Input() public resource: any;
+  @Input()
+  get cron(): string {
+    return this.localCron;
+  }
+  set cron(value: string) {
+    this.localCron = value;    
+    this.onChange(this.localCron);
+    if(this.cronChange)
+    {
+      this.cronChange.emit(this.localCron)
+    }
+  }
+  //@Input() onRefreshCron = new EventEmitter<string>();
   // the name is an Angular convention, @Input variable name + "Change" suffix
   @Output() cronChange = new EventEmitter<string>();
 
@@ -41,19 +57,7 @@ export class DxcCronEditorComponent implements OnInit, ControlValueAccessor {
   monthlyForm: FormGroup;
   yearlyForm: FormGroup;
   advancedForm: FormGroup;
-  @Input() public resource: any;
-  @Input()
-  get cron(): string {
-    return this.localCron;
-  }
-  set cron(value: string) {
-    this.localCron = value;    
-    this.onChange(this.localCron);
-    if(this.cronChange)
-    {
-      this.cronChange.emit(this.localCron)
-    }
-  }
+  
 
   get isCronFlavorQuartz() {
     return this.options.cronFlavor === 'quartz';
@@ -77,6 +81,15 @@ export class DxcCronEditorComponent implements OnInit, ControlValueAccessor {
 
   constructor(private fb: FormBuilder, private config : ConfigurationsetupService) { 
    // this.resources = config.configservice.Resources;
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes?.cronType?.currentValue !=changes?.cronType?.previousValue)
+    {
+      if(this.advancedForm && this.advancedForm.get('subTab'))
+      {
+        this.advancedForm.get('subTab').setValue(changes?.cronType?.currentValue ? changes?.cronType?.currentValue : 'expression'); 
+      }
+    }
   }
 
   /* Update the cron output to that of the selected tab.
@@ -124,9 +137,6 @@ export class DxcCronEditorComponent implements OnInit, ControlValueAccessor {
       minutes: [1],
       seconds: [0]
     });
-    
-   
-
     this.advancedForm = this.fb.group({
       subTab: ['expression'],
       expression: [this.cron != null && this.cron !='' ? this.cron : this.isCronFlavorQuartz ? '0 15 10 L-2 * ? *' : '15 10 2 * *'],
@@ -147,10 +157,13 @@ export class DxcCronEditorComponent implements OnInit, ControlValueAccessor {
         hourType: ['*']
       })
     });
+    this.cron = this.advancedForm.value.expression;
     this.advancedForm.valueChanges.subscribe(next => this.computeAdvancedExpression(next));
   }
 
   private computeAdvancedExpression(state: any) {
+    this.cronType = state.subTab;
+    this.cronTypeChange.emit(state.subTab);
     switch (state.subTab) {
       case 'expression':
         this.cron = state.expression;
@@ -476,6 +489,10 @@ export class DxcCronEditorComponent implements OnInit, ControlValueAccessor {
     };
   }
 
+  // public onRefreshCron()
+  // {
+  //   this.cron=this.localCron;
+  // }
 
 
   private getRange(start: number, end: number): number[] {

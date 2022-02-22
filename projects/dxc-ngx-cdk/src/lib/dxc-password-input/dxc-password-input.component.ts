0485bec852a2a629d04cffac5f507b1f0c5f7110
raquelarrojo <arrojoraquel@gmail.com>
2022-02-22 14:55:28 +0100

    Added password input types.

--- projects/dxc-ngx-cdk/src/lib/dxc-password-input/dxc-password-input.component.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-password-input/dxc-password-input.component.ts
@@ -16,6 +16,22 @@ import { DxcTextInputComponent } from "../dxc-text-input/dxc-text-input.componen
 import { CssUtils } from "../utils";
 import { DxcPasswordInputHelper } from "./dxc-password-input.helper";
 
+type Size = "small" | "medium" | "large" | "fillParent";
+type Space =
+  | "xxsmall"
+  | "xsmall"
+  | "small"
+  | "medium"
+  | "large"
+  | "xlarge"
+  | "xxlarge";
+type Margin = {
+  top?: Space;
+  bottom?: Space;
+  left?: Space;
+  right?: Space;
+};
+
 @Component({
   selector: "dxc-password-input",
   templateUrl: "./dxc-password-input.component.html",
@@ -23,19 +39,25 @@ import { DxcPasswordInputHelper } from "./dxc-password-input.helper";
 })
 export class DxcPasswordInputComponent implements OnInit, OnChanges {
   @HostBinding("class") className;
-
-  @Input()
-  label: string;
-
-  @Input()
-  name: string;
-
-  @Input()
-  value: string;
-
-  @Input()
-  helperText: string;
-
+  /**
+   * Text to be placed above the password.
+   */
+  @Input() label: string = "";
+  /**
+   * Name attribute of the input element.
+   */
+  @Input() name: string = "";
+  /**
+   * Value of the input element. If undefined, the component will be uncontrolled and the value will be managed internally by the component.
+   */
+  @Input() value: string;
+  /**
+   * Helper text to be placed above the password.
+   */
+  @Input() helperText: string = "";
+  /**
+   * If true, the password input will have an action to clear the entered value.
+   */
   @Input()
   get clearable(): boolean {
     return this._clearable;
@@ -44,30 +66,60 @@ export class DxcPasswordInputComponent implements OnInit, OnChanges {
     this._clearable = coerceBooleanProperty(value);
   }
   private _clearable = false;
-
-  @Input()
-  error = "";
-
-  @Input()
-  pattern = "";
-
-  @Input()
-  margin: Object | string;
-
-  @Input()
-  minLength: number;
-
-  @Input()
-  maxLength: number;
-
-  @Input()
-  tabIndex: number;
-
-  @Input()
-  size: string = "medium";
-
-  @Input()
-  autocomplete: string = "off";
+  /**
+   * If it is defined, the component will change its appearance, showing
+   * the error below the password input component. If it is not defined, the
+   * error messages will be managed internally, but never displayed on its own.
+   */
+  @Input() error: string = "";
+  /**
+   * Regular expression that defines the valid format allowed by the
+   * password input. This will be checked both when the input element loses the
+   * focus and while typing within it. If the string entered does not match
+   * the pattern, the onBlur and onChange functions will be called with the
+   * current value and an internal error informing that this value does not
+   * match the pattern. If the pattern is met, the error parameter of both
+   * events will be null.
+   */
+  @Input() pattern: string;
+  /**
+   * Specifies the minimun length allowed by the password input.
+   * This will be checked both when the input element loses the
+   * focus and while typing within it. If the string entered does not
+   * comply the minimum length, the onBlur and onChange functions will be called
+   * with the current value and an internal error informing that the value
+   * length does not comply the specified range. If a valid length is
+   * reached, the error parameter of both events will be null.
+   */
+  @Input() minLength: number;
+  /**
+   * Specifies the maximum length allowed by the password input.
+   * This will be checked both when the input element loses the
+   * focus and while typing within it. If the string entered does not
+   * comply the maximum length, the onBlur and onChange functions will be called
+   * with the current value and an internal error informing that the value
+   * length does not comply the specified range. If a valid length is
+   * reached, the error parameter of both events will be null.
+   */
+  @Input() maxLength: number;
+  /**
+   * HTML autocomplete attribute. Lets the user specify if any permission the user agent has to provide automated assistance in filling out the input value.
+   * Its value must be one of all the possible values of the HTML autocomplete attribute: 'on', 'off', 'email', 'username', 'new-password', ...
+   */
+  @Input() autocomplete: string = "off";
+  /**
+   * Size of the margin to be applied to the component ('xxsmall' | 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' | 'xxlarge').
+   * You can pass an object with 'top', 'bottom', 'left' and 'right' properties in order to specify different margin sizes.
+   */
+  @Input() margin: Space | Margin;
+  /**
+   * Size of the component ('small' | 'medium' | 'large' | 'fillParent').
+   */
+  @Input() size: Size = "medium";
+  /**
+   * Value of the tabindex attribute.
+   */
+  @Input() tabIndex: number = 0;
 
   defaultInputs = new BehaviorSubject<any>({
     error: "",
@@ -80,14 +132,25 @@ export class DxcPasswordInputComponent implements OnInit, OnChanges {
     clearable: false,
   });
 
-  @Output()
-  onChange = new EventEmitter<any>();
-
-  @Output()
-  onBlur = new EventEmitter<any>();
-
-  @Output()
-  onError = new EventEmitter<any>(true);
+  /**
+   * This function will be called when the user types within the input
+   * element of the component. An object including the current value and
+   * the error (if the value entered is not valid) will be passed to this
+   * function. If there is no error, error will be null.
+   * */
+  @Output() onChange = new EventEmitter<{
+    value: string;
+    error: string | null;
+  }>();
+  /**
+   * This function will be called when the input element loses the focus.
+   * An object including the input value and the error (if the value entered is
+   * not valid) will be passed to this function. If there is no error, error will be null.
+   */
+  @Output() onBlur = new EventEmitter<{
+    value: string;
+    error: string | null;
+  }>();
 
   @ViewChild("dxcInput", { static: false })
   dxcInputRef: DxcTextInputComponent;

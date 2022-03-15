355885e4ff3c8aee5b7c617bba2e3c81f7e8791e
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-03-15 18:03:35 +0100

    Fixed button tests

--- projects/dxc-ngx-cdk/src/lib/dxc-button/dxc-button.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-button/dxc-button.component.spec.ts
@@ -1,25 +1,40 @@
 import { render, fireEvent } from "@testing-library/angular";
 import { DxcButtonComponent } from "./dxc-button.component";
+import { TestBed } from "@angular/core/testing";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing";
+import { screen } from "@testing-library/dom";
+import { BackgroundProviderModule } from "../background-provider/background-provider.module";
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 
 describe("DxcButton tests", () => {
   test("should render dxc-button", async () => {
-    const { getByText } = await render(DxcButtonComponent, {
-      componentProperties: { label: "test-button" },
+    await render(`<dxc-button label="test-button"></dxc-button>`, {
+      imports: [BackgroundProviderModule],
+      componentProperties: {},
+      declarations: [DxcButtonComponent],
     });
 
-    expect(getByText("test-button"));
+    expect(screen.getByText("test-button"));
   });
 
   test("Calls correct function on click", async () => {
     const onClickFunction = jest.fn();
-    const { getByText } = await render(DxcButtonComponent, {
-      componentProperties: {
-        label: "test-button",
-        onClick: { emit: onClickFunction } as any,
-      },
-    });
-
-    const button = getByText("test-button");
+    await render(
+      `<dxc-button label="test-button" (onClick)="onClickFunction()"></dxc-button>`,
+      {
+        imports: [BackgroundProviderModule],
+        componentProperties: { onClickFunction },
+        declarations: [DxcButtonComponent],
+      }
+    );
+    const button = screen.getByText("test-button");
     fireEvent.click(button);
     expect(onClickFunction).toHaveBeenCalled();
   });

7a2e9af5275e4e16f87971ffa1ec36226e7f85e5
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-03-16 14:23:58 +0100

    Fixed progressbar tests

--- projects/dxc-ngx-cdk/src/lib/dxc-progressbar/dxc-progressbar.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-progressbar/dxc-progressbar.component.spec.ts
@@ -1,6 +1,16 @@
 import { render } from "@testing-library/angular";
 import { DxcProgressbarComponent } from "./dxc-progressbar.component";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing";
+import { TestBed } from "@angular/core/testing";
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 
 describe("DxcProgressbar tests", () => {
   test("should render dxc-progressbar", async () => {

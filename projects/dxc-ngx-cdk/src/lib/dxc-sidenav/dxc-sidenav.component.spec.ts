0b645ff7753bada5f2a41d06dd39405d15ef386e
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-03-16 14:27:51 +0100

    Fixed sidenav tests

--- projects/dxc-ngx-cdk/src/lib/dxc-sidenav/dxc-sidenav.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-sidenav/dxc-sidenav.component.spec.ts
@@ -1,18 +1,30 @@
 import { render, fireEvent } from "@testing-library/angular";
 import { DxcSidenavComponent } from "./dxc-sidenav.component";
 import { DxcSideNavModule } from "./dxc-sidenav.module";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing";
+import { TestBed } from "@angular/core/testing";
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 
 describe("DxcSidenav tests", () => {
   test("should render dxc-sidenav", async () => {
-    const dxcSidenav = await render(DxcSidenavComponent, {
-      template: `<dxc-sidenav>
-                    <dxc-sidenav-title>title</dxc-sidenav-title>
-                    <dxc-sidenav-subtitle>subtitle</dxc-sidenav-subtitle>
-                    <dxc-sidenav-link>link</dxc-sidenav-link>
-                  </dxc-sidenav>`,
-      excludeComponentDeclaration: true,
-      imports: [DxcSideNavModule],
-    });
+    const dxcSidenav = await render(
+      `<dxc-sidenav>
+        <dxc-sidenav-title>title</dxc-sidenav-title>
+        <dxc-sidenav-subtitle>subtitle</dxc-sidenav-subtitle>
+        <dxc-sidenav-link>link</dxc-sidenav-link>
+      </dxc-sidenav>`,
+      {
+        excludeComponentDeclaration: true,
+        imports: [DxcSideNavModule],
+      }
+    );
 
     expect(dxcSidenav.getByText("title"));
     expect(dxcSidenav.getByText("subtitle"));
@@ -21,14 +33,16 @@ describe("DxcSidenav tests", () => {
 
   test("dxc-sidenav link functionality", async () => {
     const onClickFunction = jest.fn();
-    const dxcSidenav = await render(DxcSidenavComponent, {
-      template: `<dxc-sidenav>
-                    <dxc-sidenav-link (onClick)="onClickFunction($event)">link</dxc-sidenav-link>
-                  </dxc-sidenav>`,
-      componentProperties: { onClickFunction },
-      excludeComponentDeclaration: true,
-      imports: [DxcSideNavModule],
-    });
+    const dxcSidenav = await render(
+      `<dxc-sidenav>
+        <dxc-sidenav-link (onClick)="onClickFunction($event)">link</dxc-sidenav-link>
+      </dxc-sidenav>`,
+      {
+        componentProperties: { onClickFunction },
+        excludeComponentDeclaration: true,
+        imports: [DxcSideNavModule],
+      }
+    );
 
     expect(dxcSidenav.getByText("link"));
     fireEvent.click(dxcSidenav.getByText("link"));

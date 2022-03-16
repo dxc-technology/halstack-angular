5df68ab3d6b02d1751fe4db727ce62399805cb49
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-03-16 14:56:03 +0100

    Fixed wizard tests

--- projects/dxc-ngx-cdk/src/lib/dxc-wizard/dxc-wizard.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-wizard/dxc-wizard.component.spec.ts
@@ -2,17 +2,29 @@ import { render, fireEvent } from "@testing-library/angular";
 import { DxcWizardComponent } from "./dxc-wizard.component";
 import { DxcWizardStepComponent } from "./dxc-wizard-step/dxc-wizard-step.component";
 import { DxcWizardModule } from "./dxc-wizard.module";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing";
+import { TestBed } from "@angular/core/testing";
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 
 describe("DxcWizardComponent tests", () => {
   test("should render dxc-wizard", async () => {
-    const { getByText } = await render(DxcWizardComponent, {
-      template: `<dxc-wizard>
-                  <dxc-wizard-step label="first-step"></dxc-wizard-step>
-                  <dxc-wizard-step label="second-step"></dxc-wizard-step>
-                </dxc-wizard>`,
-      imports: [DxcWizardModule],
-      excludeComponentDeclaration: true,
-    });
+    const { getByText } = await render(
+      `<dxc-wizard>
+        <dxc-wizard-step label="first-step"></dxc-wizard-step>
+        <dxc-wizard-step label="second-step"></dxc-wizard-step>
+      </dxc-wizard>`,
+      {
+        imports: [DxcWizardModule],
+        excludeComponentDeclaration: true,
+      }
+    );
 
     expect(getByText("first-step"));
     expect(getByText("second-step"));
@@ -20,17 +32,19 @@ describe("DxcWizardComponent tests", () => {
 
   test("click on step text", async () => {
     const onClickFunction = jest.fn((i) => null);
-    const { getByText } = await render(DxcWizardComponent, {
-      template: `<dxc-wizard (onStepClick)="onClickFunction($event)">
-                  <dxc-wizard-step label="first-step"></dxc-wizard-step>
-                  <dxc-wizard-step label="second-step"></dxc-wizard-step>
-                </dxc-wizard>`,
-      componentProperties: {
-        onClickFunction,
-      },
-      imports: [DxcWizardModule],
-      excludeComponentDeclaration: true,
-    });
+    const { getByText } = await render(
+      `<dxc-wizard (onStepClick)="onClickFunction($event)">
+        <dxc-wizard-step label="first-step"></dxc-wizard-step>
+        <dxc-wizard-step label="second-step"></dxc-wizard-step>
+      </dxc-wizard>`,
+      {
+        componentProperties: {
+          onClickFunction,
+        },
+        imports: [DxcWizardModule],
+        excludeComponentDeclaration: true,
+      }
+    );
 
     const step = getByText("first-step");
     fireEvent.click(step);
@@ -39,17 +53,19 @@ describe("DxcWizardComponent tests", () => {
 
   test("click on step text", async () => {
     const onClickFunction = jest.fn((i) => null);
-    const { getByText } = await render(DxcWizardComponent, {
-      template: `<dxc-wizard (onStepClick)="onClickFunction($event)">
-                  <dxc-wizard-step label="first-step"></dxc-wizard-step>
-                  <dxc-wizard-step label="second-step" description="step-description"></dxc-wizard-step>
-                </dxc-wizard>`,
-      componentProperties: {
-        onClickFunction,
-      },
-      imports: [DxcWizardModule],
-      excludeComponentDeclaration: true,
-    });
+    const { getByText } = await render(
+      `<dxc-wizard (onStepClick)="onClickFunction($event)">
+        <dxc-wizard-step label="first-step"></dxc-wizard-step>
+        <dxc-wizard-step label="second-step" description="step-description"></dxc-wizard-step>
+      </dxc-wizard>`,
+      {
+        componentProperties: {
+          onClickFunction,
+        },
+        imports: [DxcWizardModule],
+        excludeComponentDeclaration: true,
+      }
+    );
 
     const step = getByText("step-description");
     fireEvent.click(step);
@@ -58,17 +74,19 @@ describe("DxcWizardComponent tests", () => {
 
   test("click on step number", async () => {
     const onClickFunction = jest.fn((i) => null);
-    const { getByText } = await render(DxcWizardComponent, {
-      template: `<dxc-wizard (onStepClick)="onClickFunction($event)">
-                  <dxc-wizard-step label="first-step"></dxc-wizard-step>
-                  <dxc-wizard-step label="second-step" description="step-description"></dxc-wizard-step>
-                </dxc-wizard>`,
-      componentProperties: {
-        onClickFunction,
-      },
-      imports: [DxcWizardModule],
-      excludeComponentDeclaration: true,
-    });
+    const { getByText } = await render(
+      `<dxc-wizard (onStepClick)="onClickFunction($event)">
+        <dxc-wizard-step label="first-step"></dxc-wizard-step>
+        <dxc-wizard-step label="second-step" description="step-description"></dxc-wizard-step>
+      </dxc-wizard>`,
+      {
+        componentProperties: {
+          onClickFunction,
+        },
+        imports: [DxcWizardModule],
+        excludeComponentDeclaration: true,
+      }
+    );
 
     const step = getByText("1");
     fireEvent.click(step);
@@ -77,17 +95,19 @@ describe("DxcWizardComponent tests", () => {
 
   test("click on disable step", async () => {
     const onClickFunction = jest.fn((i) => null);
-    const { getByText } = await render(DxcWizardComponent, {
-      template: `<dxc-wizard (onStepClick)="onClickFunction($event)">
-                  <dxc-wizard-step label="first-step"></dxc-wizard-step>
-                  <dxc-wizard-step label="second-step" disabled="true"></dxc-wizard-step>
-                </dxc-wizard>`,
-      componentProperties: {
-        onClickFunction,
-      },
-      imports: [DxcWizardModule],
-      excludeComponentDeclaration: true,
-    });
+    const { getByText } = await render(
+      `<dxc-wizard (onStepClick)="onClickFunction($event)">
+          <dxc-wizard-step label="first-step"></dxc-wizard-step>
+          <dxc-wizard-step label="second-step" disabled="true"></dxc-wizard-step>
+        </dxc-wizard>`,
+      {
+        componentProperties: {
+          onClickFunction,
+        },
+        imports: [DxcWizardModule],
+        excludeComponentDeclaration: true,
+      }
+    );
 
     const step = getByText("second-step");
     fireEvent.click(step);

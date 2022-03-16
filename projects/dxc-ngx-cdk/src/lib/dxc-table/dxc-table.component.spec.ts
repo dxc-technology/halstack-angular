c12b77ad3d1ff8353301e47d432d76d4089721d0
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-03-16 14:34:09 +0100

    Fixed table tests

--- projects/dxc-ngx-cdk/src/lib/dxc-table/dxc-table.component.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-table/dxc-table.component.spec.ts
@@ -1,31 +1,43 @@
 import { render } from "@testing-library/angular";
 import { DxcTableComponent } from "./dxc-table.component";
 import { DxcTableModule } from "./dxc-table.module";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing";
+import { TestBed } from "@angular/core/testing";
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 
 describe("DxcTable tests", () => {
   test("should render dxc-table", async () => {
-    const table = await render(DxcTableComponent, {
-      template: `<dxc-table margin="medium">
-                    <tr>
-                        <th>header 1</th>
-                        <th>header 2</th>
-                        <th>header 3</th>
-                    </tr>
-                    <tr>
-                        <td>cell 1</td>
-                        <td>cell 2</td>
-                        <td>cell 3</td>
-                    </tr>
-                    <tr>
-                        <td>cell 4</td>
-                        <td>cell 5</td>
-                        <td>cell 6</td>
-                    </tr>
-                    </dxc-table>`,
-      componentProperties: {},
-      imports: [DxcTableModule],
-      excludeComponentDeclaration: true,
-    });
+    const table = await render(
+      `<dxc-table margin="medium">
+        <tr>
+            <th>header 1</th>
+            <th>header 2</th>
+            <th>header 3</th>
+        </tr>
+        <tr>
+            <td>cell 1</td>
+            <td>cell 2</td>
+            <td>cell 3</td>
+        </tr>
+        <tr>
+            <td>cell 4</td>
+            <td>cell 5</td>
+            <td>cell 6</td>
+        </tr>
+        </dxc-table>`,
+      {
+        componentProperties: {},
+        imports: [DxcTableModule],
+        excludeComponentDeclaration: true,
+      }
+    );
     table.detectChanges();
     expect(table.getByText("header 1")).toBeTruthy();
     expect(table.getByText("header 2")).toBeTruthy();

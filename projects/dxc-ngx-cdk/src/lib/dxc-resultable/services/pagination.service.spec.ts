8674bf78857c889cc0a2ff1cbdb6065e7286f9da
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2022-03-18 08:57:20 +0100

    Fixing tests on text input component and skipping in sort service

--- projects/dxc-ngx-cdk/src/lib/dxc-resultable/services/pagination.service.spec.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-resultable/services/pagination.service.spec.ts
@@ -1,5 +1,14 @@
-import { TestBed } from "@angular/core/testing";
 import { PaginationService } from "./pagination.service";
+import { TestBed } from "@angular/core/testing";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing";
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 
 describe("PaginationService", () => {
   let service: PaginationService;
--- projects/dxc-ngx-cdk/src/lib/dxc-resultable/services/sort.service.ts
+++ projects/dxc-ngx-cdk/src/lib/dxc-resultable/services/sort.service.ts
@@ -50,6 +50,7 @@ export class SortService {
 
   /** Set default icon to the header. */
   setDefaultIconSort(el: ElementRef) {
+    console.log(el.nativeElement);
     let columnName = el.nativeElement.id;
     el.nativeElement.children[0].innerHTML = this.getDefaultIcon(columnName);
   }
@@ -66,17 +67,17 @@ export class SortService {
   }
 
   /** Return default icon for the given header. */
-  private getDefaultIcon(columnName: string) {
+  getDefaultIcon(columnName: string) {
     return `<svg id="icon_default-${columnName}" class="iconHeader" xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 24 24" width="24" fill="white"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 5.83L15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z"/></svg>`;
   }
 
   /** Return asc icon for the given header. */
-  private getAscIcon(columnName: string) {
+  getAscIcon(columnName: string) {
     return `<svg id="icon_asc-${columnName}" class="iconHeader" xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 24 24" width="24" fill="white"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>`;
   }
 
   /** Return desc icon for the given header. */
-  private getDescIcon(columnName: string) {
+  getDescIcon(columnName: string) {
     return `<svg id="icon_desc-${columnName}" class="iconHeader" xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 24 24" width="24" fill="white"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/></svg>`;
   }
 }

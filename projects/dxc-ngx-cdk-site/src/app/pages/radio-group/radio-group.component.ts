183a6f6b746a81540c0a9534ea2a3677e16a48d1
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-03-29 09:02:59 +0200

    Added examples

--- projects/dxc-ngx-cdk-site/src/app/pages/radio-group/radio-group.component.ts
+++ projects/dxc-ngx-cdk-site/src/app/pages/radio-group/radio-group.component.ts
@@ -1,27 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Section } from '../../model/sections';
-import { RadioGroupApiComponent } from '../../components/examples/radio-group/radio-group-api/radio-group-api.component';
-// import { RadioExampleComponent } from '../../components/examples/radio-group/radio-group-example/radio-group-example.component';
+import { Component, OnInit } from "@angular/core";
+import { Section } from "../../model/sections";
+import { RadioGroupApiComponent } from "../../components/examples/radio-group/radio-group-api/radio-group-api.component";
+import { RadioGroupExampleComponent } from "../../components/examples/radio-group/radio-group-example/radio-group-example.component";
 
 @Component({
-  selector: 'app-radio-group',
-  templateUrl: './radio-group.component.html',
-  styleUrls: ['./radio-group.component.scss']
+  selector: "app-radio-group",
+  templateUrl: "./radio-group.component.html",
+  styleUrls: ["./radio-group.component.scss"],
 })
 export class RadioGroupComponent implements OnInit {
   sections: Array<Section>;
   tagImg: string;
 
-  constructor(){
+  constructor() {
     this.sections = new Array<Section>();
-    this.tagImg = './assets/img/ready.svg';
+    this.tagImg = "./assets/img/ready.svg";
   }
 
   ngOnInit() {
-    this.sections.push( 
-      {id: 0, label: 'API',selector: 'radio-group-api', component: RadioGroupApiComponent},
-      // {id: 1,label: 'EXAMPLES', selector: 'examples-component-radio-group', component: RadioGroupExampleComponent}
-      );
+    this.sections.push(
+      {
+        id: 0,
+        label: "API",
+        selector: "radio-group-api",
+        component: RadioGroupApiComponent,
+      },
+      {
+        id: 1,
+        label: "EXAMPLES",
+        selector: "examples-component-radio-group",
+        component: RadioGroupExampleComponent,
+      }
+    );
   }
-
 }

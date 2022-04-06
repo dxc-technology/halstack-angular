c7fe993f1bf7688985fd53bf9234c54aadfaea07
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-04-06 15:47:50 +0200

    Added types file in heading

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-heading/dxc-heading.types.ts
@@ -0,0 +1,23 @@
+export interface LinkProperties {
+  margin?: Space | Spacing;
+  level: 1 | 2 | 3 | 4 | 5;
+  asTag: "h1" | "h2" | "h3" | "h4" | "h5";
+  text: string;
+  weight: "light" | "normal" | "bold";
+}
+
+export type Space =
+  | "xxsmall"
+  | "xsmall"
+  | "small"
+  | "medium"
+  | "large"
+  | "xlarge"
+  | "xxlarge";
+
+export type Spacing = {
+  top?: Space;
+  bottom?: Space;
+  left?: Space;
+  right?: Space;
+};

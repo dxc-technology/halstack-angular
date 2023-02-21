9e0a65583a661731a6995b159a72ad23185f4629
Shivam Anand <shivam.anand@dxc.com>
2023-02-21 15:51:54 +0530

    feature/RMA-100989 : checkin on behalf of sachin

---
+++ projects/dxc-ngx-cdk/src/lib/dxc-dialog/props/dailog-props.ts
@@ -0,0 +1,21 @@
+export enum MODEL_HEIGHT {
+    xs = 30,
+    esm = 40,
+    sm = 50,
+    md = 60, 
+    emd = 70, 
+    lg = 80, 
+    elg = 90, 
+    xl = 100
+}
+
+export enum MODEL_WIDTH {
+    xs = 30,
+    esm = 40,
+    sm = 50,
+    md = 60, 
+    emd = 70, 
+    lg = 80, 
+    elg = 90, 
+    xl = 100
+}
